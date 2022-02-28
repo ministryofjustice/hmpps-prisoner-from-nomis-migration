236f09ab068a368f1a801cd0804b5d0708931b65
Peter Phillips <peter@greenthistle.co.uk>
2022-02-28 08:46:47 +0000

    SDI-71: 📝 Copy date time fix and security improvements

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,11 +13,12 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.0.5")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.0.6")
 
   implementation("org.springdoc:springdoc-openapi-ui:1.6.6")
   implementation("org.springdoc:springdoc-openapi-kotlin:1.6.6")
   implementation("org.springdoc:springdoc-openapi-data-rest:1.6.6")
+  implementation("org.springdoc:springdoc-openapi-security:1.6.6")
 
   testImplementation("io.swagger.parser.v3:swagger-parser:2.0.30")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
