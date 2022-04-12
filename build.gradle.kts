8cfcdf9790cdeeb070b4425463a9490435b90c1e
Peter Phillips <peter@greenthistle.co.uk>
2022-04-12 15:09:09 +0100

    SDI-155: 🔒️ Fix security issues by bumping versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.1.3"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.1.4-beta"
   kotlin("plugin.spring") version "1.6.20"
 }
 
@@ -36,9 +36,9 @@ dependencies {
   testImplementation("io.swagger.parser.v3:swagger-parser:2.0.32")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
-  testImplementation("org.testcontainers:localstack:1.16.3")
+  testImplementation("org.testcontainers:localstack:1.17.0")
   testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
-  testImplementation("org.testcontainers:postgresql:1.16.3")
+  testImplementation("org.testcontainers:postgresql:1.17.0")
 }
 
 java {
