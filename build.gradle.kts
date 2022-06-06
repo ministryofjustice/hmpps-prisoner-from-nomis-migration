c9ee38a7750bc97b996447ad22fd88bb5f603563
petergphillips <peter@greenthistle.co.uk>
2022-06-06 11:58:16 +0100

    SDI-218: ⬆️a Use latest plugin with suppressions (#91)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.2.0-beta-4"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.2.1"
   kotlin("plugin.spring") version "1.6.21"
 }
 
@@ -20,18 +20,18 @@ dependencies {
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.1.3")
   implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
 
-  implementation("org.springdoc:springdoc-openapi-ui:1.6.8")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.8")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.8")
-  implementation("org.springdoc:springdoc-openapi-security:1.6.8")
+  implementation("org.springdoc:springdoc-openapi-ui:1.6.9")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.9")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.9")
+  implementation("org.springdoc:springdoc-openapi-security:1.6.9")
 
-  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
-  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.6.1")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.2")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.6.2")
 
   runtimeOnly("io.r2dbc:r2dbc-postgresql:0.8.12.RELEASE")
   runtimeOnly("org.springframework.boot:spring-boot-starter-jdbc")
   runtimeOnly("org.postgresql:postgresql:42.3.6")
-  implementation("org.flywaydb:flyway-core:8.5.11")
+  implementation("org.flywaydb:flyway-core:8.5.12")
 
   testImplementation("io.swagger.parser.v3:swagger-parser:2.0.33")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
