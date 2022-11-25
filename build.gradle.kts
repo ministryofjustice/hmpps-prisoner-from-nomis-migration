6d8c421efe3d19e123ac843ab4cde4b4b9fa0511
petergphillips <peter@greenthistle.co.uk>
2022-11-25 15:09:50 +0000

    SDI-463: ⬆️  Upgrade to latest gradle spring boot (#195)
    * SDI-463: ⬆️  Upgrade to latest gradle spring boot
    
    * SDI-463: 🐛 Temp downgrade r2dbc postgres

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.6.0-beta"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.7.0-beta"
   kotlin("plugin.spring") version "1.7.21"
 }
 
@@ -15,6 +15,9 @@ configurations {
   testImplementation { exclude(group = "org.junit.vintage") }
 }
 
+// Temporarily kept at 0.9.2 as get class java.lang.Long cannot be cast to class java.lang.Integer when upgrading to 1.0.0.RELEASE
+val r2dbcPostgresVersion by extra("0.9.2.RELEASE")
+
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-security")
@@ -23,27 +26,27 @@ dependencies {
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.1.13")
   implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
 
-  implementation("org.springdoc:springdoc-openapi-webflux-ui:1.6.12")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.12")
-  implementation("org.springdoc:springdoc-openapi-security:1.6.12")
+  implementation("org.springdoc:springdoc-openapi-webflux-ui:1.6.13")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.13")
+  implementation("org.springdoc:springdoc-openapi-security:1.6.13")
 
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.6.4")
   implementation("io.jsonwebtoken:jjwt:0.9.1")
-  implementation("io.opentelemetry:opentelemetry-api:1.20.0")
-  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.0")
+  implementation("io.opentelemetry:opentelemetry-api:1.20.1")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.1")
 
-  runtimeOnly("org.postgresql:r2dbc-postgresql:0.9.2.RELEASE")
+  runtimeOnly("org.postgresql:r2dbc-postgresql:$r2dbcPostgresVersion")
   runtimeOnly("org.springframework.boot:spring-boot-starter-jdbc")
-  runtimeOnly("org.postgresql:postgresql:42.5.0")
+  runtimeOnly("org.postgresql:postgresql:42.5.1")
   implementation("org.flywaydb:flyway-core")
 
-  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.8")
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.9")
 
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
-  testImplementation("org.testcontainers:localstack:1.17.5")
+  testImplementation("org.testcontainers:localstack:1.17.6")
   testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
-  testImplementation("org.testcontainers:postgresql:1.17.5")
+  testImplementation("org.testcontainers:postgresql:1.17.6")
   testImplementation("io.mockk:mockk:1.13.2")
 }
 
