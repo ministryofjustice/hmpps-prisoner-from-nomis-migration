263b0b1d087e58c1d572be2264747f958fb6000f
lbennett-moj <lucy.bennett@digital .justice.gov.uk>
2022-12-02 10:04:29 +0000

    sdi-474 create time edge case

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/service/NomisApiService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/service/NomisApiService.kt
@@ -228,7 +228,7 @@ data class NomisIncentive(
      We will use the seconds from the 'when created' timestamp to avoid multiple IEP records created in the same minute being out of order.
    */
   private fun getTransformedIncentiveDateTime(): LocalDateTime =
-    if (iepDateTime.minute < whenCreated.minute) {
+    if (iepDateTime.minute != whenCreated.minute) {
       iepDateTime.withSecond(59)
     } else
       iepDateTime.withSecond(whenCreated.second)
