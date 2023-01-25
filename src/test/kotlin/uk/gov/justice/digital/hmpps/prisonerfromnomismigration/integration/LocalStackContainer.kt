1feeb380918392b18220a2172db9876e0dbb57bd
Andy Marke <andy.marke@digital.justice.gov.uk>
2023-01-25 17:08:59 +0000

    SDI-518 Upgrade localstack

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/integration/LocalStackContainer.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/integration/LocalStackContainer.kt
@@ -23,7 +23,7 @@ object LocalStackContainer {
     if (localstackIsRunning()) return null
     val logConsumer = Slf4jLogConsumer(log).withPrefix("localstack")
     return LocalStackContainer(
-      DockerImageName.parse("localstack/localstack").withTag("0.14.2")
+      DockerImageName.parse("localstack/localstack").withTag("1.2.0")
     ).apply {
       withServices(LocalStackContainer.Service.SQS)
       withEnv("HOSTNAME_EXTERNAL", "localhost")
