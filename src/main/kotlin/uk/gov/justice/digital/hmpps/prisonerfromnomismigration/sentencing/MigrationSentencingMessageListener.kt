05cb8fe87a8c6951d502a16fb55f467d22078c69
petergphillips <peter@greenthistle.co.uk>
2023-02-02 08:57:57 +0000

    SDI-593: ✨ Log listener events as requests (#221)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/sentencing/MigrationSentencingMessageListener.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/sentencing/MigrationSentencingMessageListener.kt
@@ -34,7 +34,7 @@ class MigrationSentencingMessageListener(
   }
 
   @SqsListener(SENTENCING_QUEUE_ID, factory = "hmppsQueueContainerFactoryProxy")
-  @WithSpan(value = "sqs-migration-sentencing-queue", kind = SpanKind.SERVER)
+  @WithSpan(value = "dps-syscon-migration_sentencing_queue", kind = SpanKind.SERVER)
   fun onMessage(message: String, rawMessage: Message): CompletableFuture<Void>? {
     log.debug("Received message {}", message)
     val migrationMessage: MigrationMessage<SentencingMessages, *> = message.fromJson()
