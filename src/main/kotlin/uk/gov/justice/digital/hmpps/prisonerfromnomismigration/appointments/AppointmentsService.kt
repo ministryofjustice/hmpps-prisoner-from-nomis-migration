dbb1065fb23505547696172afd77d72d72fcf655
Steve Rendell <32732937+steverendell@users.noreply.github.com>
2023-05-12 13:38:25 +0100

    SDIT-800 Link up the activities api migrate appointments endpoint (#290)
    * SDIT-800 Link up the activities api migrate appointments endpoint
    
    * SDIT-800 Link up the activities api migrate appointments endpoint

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/appointments/AppointmentsService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/appointments/AppointmentsService.kt
@@ -1,7 +1,6 @@
 package uk.gov.justice.digital.hmpps.prisonerfromnomismigration.appointments
 
 import com.fasterxml.jackson.annotation.JsonFormat
-import jakarta.validation.constraints.NotEmpty
 import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Size
 import org.springframework.beans.factory.annotation.Qualifier
@@ -9,6 +8,7 @@ import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.awaitBody
 import java.time.LocalDate
+import java.time.LocalDateTime
 import java.time.LocalTime
 
 @Service
@@ -27,33 +27,41 @@ data class CreateAppointmentResponse(
 
 data class AppointmentMigrateRequest(
 
-  @field:NotEmpty(message = "Prison code must be supplied")
   @field:Size(max = 6, message = "Prison code should not exceed {max} characters")
-  val prisonCode: String?,
+  val prisonCode: String,
 
-  @field:NotEmpty(message = "Prisoner number must be supplied")
   @field:Size(max = 10, message = "Prisoner number should not exceed {max} characters")
-  val prisonerNumber: String?,
+  val prisonerNumber: String,
 
   @field:NotNull(message = "Booking ID must be supplied")
   val bookingId: Long?,
 
-  @field:NotEmpty(message = "Category code must be supplied")
   @field:Size(max = 12, message = "Category code should not exceed {max} characters")
-  val categoryCode: String?,
+  val categoryCode: String,
 
   val internalLocationId: Long?,
 
-  @field:NotNull(message = "Start date must be supplied")
   @JsonFormat(pattern = "yyyy-MM-dd")
-  val startDate: LocalDate?,
+  val startDate: LocalDate,
 
-  @field:NotNull(message = "Start time must be supplied")
   @JsonFormat(pattern = "HH:mm")
-  val startTime: LocalTime?,
+  val startTime: LocalTime,
 
   @JsonFormat(pattern = "HH:mm")
   val endTime: LocalTime?,
 
-  val comment: String = "",
+  val comment: String?,
+  val isCancelled: Boolean?,
+
+  @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+  val created: LocalDateTime,
+
+  @field:Size(max = 100, message = "Created by should not exceed {max} characters")
+  val createdBy: String,
+
+  @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+  val updated: LocalDateTime?,
+
+  @field:Size(max = 100, message = "Updated by should not exceed {max} characters")
+  val updatedBy: String?,
 )
--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/service/NomisApiService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/service/NomisApiService.kt
@@ -209,24 +209,33 @@ data class NomisAdjustment(
 data class AppointmentResponse(
   val bookingId: Long,
   val offenderNo: String,
-  val prisonId: String? = null,
+  val prisonId: String, // prison or toPrison is never null in existing nomis data for event_type = 'APP' (as at 11/5/2023)
   val internalLocation: Long? = null,
   val startDateTime: LocalDateTime? = null,
   val endDateTime: LocalDateTime? = null,
   val comment: String? = null,
   val subtype: String,
   val status: String,
+  val createdDate: LocalDateTime,
+  val createdBy: String,
+  val modifiedDate: LocalDateTime? = null,
+  val modifiedBy: String? = null,
 ) {
   fun toAppointment() = AppointmentMigrateRequest(
     bookingId = bookingId,
     prisonerNumber = offenderNo,
     prisonCode = prisonId,
     internalLocationId = internalLocation,
-    startDate = startDateTime?.toLocalDate(),
-    startTime = startDateTime?.toLocalTime(),
+    startDate = startDateTime!!.toLocalDate(), // never null in existing nomis data for event_type = 'APP' (as at 11/5/2023)
+    startTime = startDateTime.toLocalTime(),
     endTime = endDateTime?.toLocalTime(),
-    comment = comment ?: "",
+    comment = comment,
     categoryCode = subtype,
+    isCancelled = status == "CANC",
+    created = createdDate,
+    createdBy = createdBy,
+    updated = modifiedDate,
+    updatedBy = modifiedBy,
   )
 }
 
