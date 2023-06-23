9c45c42bc6cd688e4600699bbad022e64f2057ff
petergphillips <peter@greenthistle.co.uk>
2023-06-23 15:22:58 +0100

    SDIT-903: ⬆️  Upgrade to latest spring boot for netty cve (#305)

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ import org.jlleitschuh.gradle.ktlint.tasks.KtLintFormatTask
 import org.openapitools.generator.gradle.plugin.tasks.GenerateTask
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.2.0"
-  kotlin("plugin.spring") version "1.8.21"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.2.1"
+  kotlin("plugin.spring") version "1.8.22"
   id("org.openapi.generator") version "6.6.0"
 }
 
@@ -49,7 +49,7 @@ dependencies {
   runtimeOnly("org.postgresql:postgresql:42.6.0")
   implementation("org.flywaydb:flyway-core")
 
-  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:1.26.0")
+  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:1.27.0")
 
   testImplementation("io.swagger.parser.v3:swagger-parser:2.1.15")
   testImplementation("io.jsonwebtoken:jjwt-impl:0.11.5")
@@ -57,7 +57,7 @@ dependencies {
 
   testImplementation("com.github.tomakehurst:wiremock-jre8-standalone:2.35.0")
   testImplementation("org.testcontainers:localstack:1.18.3")
-  testImplementation("com.amazonaws:aws-java-sdk-core:1.12.481")
+  testImplementation("com.amazonaws:aws-java-sdk-core:1.12.494")
   testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
   testImplementation("org.testcontainers:postgresql:1.18.3")
   testImplementation("io.mockk:mockk:1.13.5")
