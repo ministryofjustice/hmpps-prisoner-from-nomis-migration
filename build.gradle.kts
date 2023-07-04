f6a66bcc30a1d4c210b0497a7b2d3454caa92417
Steve Rendell <32732937+steverendell@users.noreply.github.com>
2023-07-04 09:30:26 +0100

    SDIT-865 Improve Get DLQ API in SQS lib (#308)
    upgrade to test change
    
    Co-authored-by: steverendell <steve.rendell@syscon.net>

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:2.0.0")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:2.0.1")
   implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
 
   implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.1.0")
