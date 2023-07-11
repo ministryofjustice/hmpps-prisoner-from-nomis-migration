58e0b3290535d1ef0f6e7683bac308cd9553c190
lbennett-moj <lucy.bennett@digital .justice.gov.uk>
2023-07-11 16:02:04 +0100

    sdit-927 further adjudication migration tests and common code sharing

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/integration/SqsIntegrationTestBase.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/integration/SqsIntegrationTestBase.kt
@@ -90,6 +90,7 @@ class SqsIntegrationTestBase : TestBase() {
       sentencingOffenderEventsQueue,
       adjudicationsMigrationQueue,
       appointmentsMigrationQueue,
+      sentencingMigrationQueue,
     )
   }
 
