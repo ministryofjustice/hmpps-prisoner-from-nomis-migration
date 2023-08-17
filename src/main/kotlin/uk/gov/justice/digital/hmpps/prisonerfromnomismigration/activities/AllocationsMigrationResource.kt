985d90b7e2737fd70a36b10b9a776a7ef9ab0552
Mike Halma <mike.halma@digital.justice.gov.uk>
2023-08-17 10:13:43 +0100

    SDIT-984 Allocation migration history API

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/activities/AllocationsMigrationResource.kt
@@ -0,0 +1,85 @@
+package uk.gov.justice.digital.hmpps.prisonerfromnomismigration.activities
+
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.media.ArraySchema
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import org.springframework.format.annotation.DateTimeFormat
+import org.springframework.http.MediaType
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.config.ErrorResponse
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.persistence.repository.MigrationHistory
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.service.HistoryFilter
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.service.MigrationHistoryService
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.service.MigrationType
+import java.time.LocalDateTime
+
+@RestController
+@RequestMapping("/migrate", produces = [MediaType.APPLICATION_JSON_VALUE])
+class AllocationsMigrationResource(
+  private val migrationHistoryService: MigrationHistoryService,
+) {
+  @PreAuthorize("hasRole('ROLE_MIGRATE_ACTIVITIES')")
+  @GetMapping("/allocations/history")
+  @Operation(
+    summary = "Lists all filtered migration history records un-paged for activities",
+    description = "The records are un-paged and requires role <b>MIGRATE_ACTIVITIES</b>",
+    responses = [
+      ApiResponse(
+        responseCode = "200",
+        description = "All migration history records",
+        content = [
+          Content(
+            mediaType = "application/json",
+            array = ArraySchema(schema = Schema(implementation = MigrationHistory::class)),
+          ),
+        ],
+      ),
+      ApiResponse(
+        responseCode = "401",
+        description = "Unauthorized to access this endpoint",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "403",
+        description = "Incorrect permissions to access this endpoint",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+    ],
+  )
+  suspend fun getAll(
+    @Parameter(
+      description = "Only include migrations started after this date time",
+      example = "2020-03-23T12:00:00",
+    )
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @RequestParam
+    fromDateTime: LocalDateTime? = null,
+
+    @Parameter(
+      description = "Only include migrations started before this date time",
+      example = "2020-03-24T12:00:00",
+    )
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @RequestParam
+    toDateTime: LocalDateTime? = null,
+
+    @Parameter(
+      description = "When true only include migrations that had at least one failure",
+      example = "false",
+    ) @RequestParam includeOnlyFailures: Boolean = false,
+  ) = migrationHistoryService.findAll(
+    HistoryFilter(
+      migrationTypes = listOf(MigrationType.ALLOCATIONS.name),
+      fromDateTime = fromDateTime,
+      toDateTime = toDateTime,
+      includeOnlyFailures = includeOnlyFailures,
+    ),
+  )
+}
