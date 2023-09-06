ab471a074c1950688dbe5d9d3fad83e9d1becfe3
Andy Marke <andy.marke@digital.justice.gov.uk>
2023-09-06 09:01:59 +0100

    SDIT-928 Use mock for today and simulate a full mapping

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/adjudications/MockAdjudicationsResource.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/adjudications/MockAdjudicationsResource.kt
@@ -10,7 +10,10 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.adjudications.model.AdjudicationMigrateDto
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.adjudications.model.ChargeNumberMapping
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.adjudications.model.HearingMapping
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.adjudications.model.MigrateResponse
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.adjudications.model.PunishmentMapping
+import kotlin.random.Random
 
 /**
  * This represents the possible interface for the adjudications service.
@@ -32,12 +35,23 @@ class MockAdjudicationsResource {
     log.info("Created adjudication for migration with id ${adjudicationRequest.oicIncidentId} for offender no ${adjudicationRequest.prisoner.prisonerNumber}. Request was $adjudicationRequest")
     return MigrateResponse(
       chargeNumberMapping = ChargeNumberMapping(
-        "${adjudicationRequest.oicIncidentId}/${adjudicationRequest.offenceSequence}",
+        "${adjudicationRequest.oicIncidentId}-${adjudicationRequest.offenceSequence}",
         oicIncidentId = adjudicationRequest.oicIncidentId,
         offenceSequence = adjudicationRequest.offenceSequence,
       ),
-      hearingMappings = emptyList(),
-      punishmentMappings = emptyList(),
+      hearingMappings = adjudicationRequest.hearings.map {
+        HearingMapping(
+          hearingId = Random.nextLong(),
+          oicHearingId = it.oicHearingId,
+        )
+      },
+      punishmentMappings = adjudicationRequest.punishments.map {
+        PunishmentMapping(
+          punishmentId = Random.nextLong(),
+          bookingId = adjudicationRequest.bookingId,
+          sanctionSeq = it.sanctionSeq,
+        )
+      },
     )
   }
 }
