98b57540619cf3cc285d58ffec313c1ad16e6754
Peter Phillips <peter@greenthistle.co.uk>
2023-10-11 16:39:24 +0100

    SDIT-1142: 🔒️ Also remove jjwt workaround

--- build.gradle.kts
+++ build.gradle.kts
@@ -73,11 +73,6 @@ tasks {
     // Under gradle 8 we must declare the dependency here, even if we're not going to be linting the model
     mustRunAfter("buildActivityApiModel", "buildNomisSyncApiModel", "buildAdjudicationApiModel", "buildNonAssociationsApiModel", "buildMappingServiceApiModel")
   }
-
-  test {
-    // required for jjwt 0.12 - see https://github.com/jwtk/jjwt/issues/849
-    jvmArgs("--add-exports", "java.base/sun.security.util=ALL-UNNAMED")
-  }
 }
 
 tasks.register("buildActivityApiModel", GenerateTask::class) {
