f74c9605bab2e5054f1d4fdd5e3dc2847d9cf3a1
Andy Marke <andy.marke@digital.justice.gov.uk>
2023-11-15 16:17:25 +0000

    SDIT-1247 Match punishments

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/adjudications/AdjudicationMappingCreatorTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/adjudications/AdjudicationMappingCreatorTest.kt
@@ -15,6 +15,12 @@ import org.junit.jupiter.api.assertThrows
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Import
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.adjudications.model.HearingMapping
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.adjudications.model.PunishmentDto
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.adjudications.model.PunishmentDto.Type.ADDITIONAL_DAYS
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.adjudications.model.PunishmentDto.Type.CONFINEMENT
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.adjudications.model.PunishmentDto.Type.EXCLUSION_WORK
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.adjudications.model.PunishmentDto.Type.PROSPECTIVE_DAYS
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.adjudications.model.PunishmentMapping
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.helper.SpringAPIServiceTest
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomissync.model.AdjudicationCharge
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomissync.model.AdjudicationChargeResponse
@@ -22,6 +28,8 @@ import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomissync.model.A
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomissync.model.AdjudicationOffence
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomissync.model.CodeDescription
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomissync.model.Hearing
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomissync.model.HearingResult
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomissync.model.HearingResultAward
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomissync.model.InternalLocation
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomissync.model.Staff
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.wiremock.AdjudicationsApiExtension.Companion.adjudicationsApi
@@ -174,7 +182,7 @@ class AdjudicationMappingCreatorTest {
         assertThat(mapping?.chargeNumberMapping?.chargeNumber).isEqualTo("12345-1")
         assertThat(mapping?.chargeNumberMapping?.oicIncidentId).isEqualTo(12345)
         assertThat(mapping?.chargeNumberMapping?.offenceSequence).isEqualTo(1)
-        assertThat(mapping?.hearingMappings).containsExactly(HearingMapping(hearingId = 1, oicHearingId = 11))
+        assertThat(mapping?.hearingMappings).containsExactlyInAnyOrder(HearingMapping(hearingId = 1, oicHearingId = 11))
         assertThat(mapping?.punishmentMappings).isEmpty()
       }
 
@@ -227,7 +235,7 @@ class AdjudicationMappingCreatorTest {
         assertThat(mapping?.chargeNumberMapping?.chargeNumber).isEqualTo("12345-1")
         assertThat(mapping?.chargeNumberMapping?.oicIncidentId).isEqualTo(12345)
         assertThat(mapping?.chargeNumberMapping?.offenceSequence).isEqualTo(1)
-        assertThat(mapping?.hearingMappings).containsExactly(
+        assertThat(mapping?.hearingMappings).containsExactlyInAnyOrder(
           HearingMapping(hearingId = 1, oicHearingId = 11),
           HearingMapping(hearingId = 2, oicHearingId = 12),
           HearingMapping(hearingId = 3, oicHearingId = 13),
@@ -252,11 +260,133 @@ class AdjudicationMappingCreatorTest {
         }
       }
     }
+
+    @Nested
+    inner class WhenDPSHasMultiplePunishments {
+      @BeforeEach
+      fun setUp() {
+        adjudicationsApi.stubChargeGet(
+          chargeNumber = "12345-1",
+          hearings = """
+            [
+              ${dpsHearing(1, LocalDateTime.parse("2021-08-23T14:25:00"))},
+              ${dpsHearing(2, LocalDateTime.parse("2023-08-23T14:25:00"))}
+            ]""",
+          punishments = """
+            [
+              ${dpsPunishment(1, 10, EXCLUSION_WORK)},
+              ${dpsPunishment(2, 365, EXCLUSION_WORK)},
+              ${dpsPunishment(3, 8, ADDITIONAL_DAYS)},
+              ${dpsPunishment(4, 8, CONFINEMENT)},
+              ${dpsPunishment(5, 8, PROSPECTIVE_DAYS)},
+              ${dpsPunishment(6, 2, EXCLUSION_WORK)}
+            ]""",
+        )
+      }
+
+      @Test
+      fun `will map punishments`() = runTest {
+        val mapping = adjudicationMappingCreator.createMigrationMappingResponse(
+          nomisAdjudication(
+            bookingId = 123456,
+            adjudicationNumber = 12345,
+            chargeSequence = 1,
+            hearings = listOf(
+              nomisHearing(
+                hearingId = 11,
+                hearingDateTime = LocalDateTime.parse("2021-08-23T14:25:00"),
+                awards = listOf(nomisAward(sequence = 106, nomisTypeCode = "EXTRA_WORK", sanctionDays = 2)),
+              ),
+              nomisHearing(
+                hearingId = 12,
+                hearingDateTime = LocalDateTime.parse("2023-08-23T14:25:00"),
+                awards = listOf(
+                  nomisAward(
+                    sequence = 102,
+                    nomisTypeCode = "EXTRA_WORK",
+                    sanctionMonths = 12,
+                    effectiveDate = LocalDate.parse("2021-01-01"),
+                  ),
+                  nomisAward(sequence = 104, nomisTypeCode = "CC", sanctionDays = 8),
+                  nomisAward(
+                    sequence = 105,
+                    nomisTypeCode = "ADA",
+                    sanctionDays = 8,
+                    nomisSanctionStatus = "PROSPECTIVE",
+                  ),
+                  nomisAward(sequence = 101, nomisTypeCode = "EXTRA_WORK", sanctionDays = 10),
+                  nomisAward(sequence = 103, nomisTypeCode = "ADA", sanctionDays = 8),
+                ),
+              ),
+            ),
+          ),
+        )
+        assertThat(mapping).isNotNull
+        assertThat(mapping?.chargeNumberMapping?.chargeNumber).isEqualTo("12345-1")
+        assertThat(mapping?.chargeNumberMapping?.oicIncidentId).isEqualTo(12345)
+        assertThat(mapping?.chargeNumberMapping?.offenceSequence).isEqualTo(1)
+        assertThat(mapping?.hearingMappings).containsExactlyInAnyOrder(
+          HearingMapping(hearingId = 1, oicHearingId = 11),
+          HearingMapping(hearingId = 2, oicHearingId = 12),
+        )
+        assertThat(mapping?.punishmentMappings).containsExactlyInAnyOrder(
+          PunishmentMapping(punishmentId = 1, sanctionSeq = 101, bookingId = 123456),
+          PunishmentMapping(punishmentId = 2, sanctionSeq = 102, bookingId = 123456),
+          PunishmentMapping(punishmentId = 3, sanctionSeq = 103, bookingId = 123456),
+          PunishmentMapping(punishmentId = 4, sanctionSeq = 104, bookingId = 123456),
+          PunishmentMapping(punishmentId = 5, sanctionSeq = 105, bookingId = 123456),
+          PunishmentMapping(punishmentId = 6, sanctionSeq = 106, bookingId = 123456),
+        )
+      }
+
+      @Test
+      fun `will throw exception when NOMIS has a different number of punishments`() = runTest {
+        assertThrows<IllegalStateException> {
+          adjudicationMappingCreator.createMigrationMappingResponse(
+            nomisAdjudication(
+              adjudicationNumber = 12345,
+              chargeSequence = 1,
+              hearings = listOf(
+                nomisHearing(
+                  hearingId = 11,
+                  hearingDateTime = LocalDateTime.parse("2021-08-23T14:25:00"),
+                  awards = listOf(),
+                ),
+                nomisHearing(
+                  hearingId = 12,
+                  hearingDateTime = LocalDateTime.parse("2023-08-23T14:25:00"),
+                  awards = listOf(
+                    nomisAward(
+                      sequence = 102,
+                      nomisTypeCode = "EXTRA_WORK",
+                      sanctionMonths = 12,
+                      effectiveDate = LocalDate.parse("2021-01-01"),
+                    ),
+                    nomisAward(sequence = 104, nomisTypeCode = "CC", sanctionDays = 8),
+                    nomisAward(
+                      sequence = 105,
+                      nomisTypeCode = "ADA",
+                      sanctionDays = 8,
+                      nomisSanctionStatus = "PROSPECTIVE",
+                    ),
+                    nomisAward(sequence = 101, nomisTypeCode = "EXTRA_WORK", sanctionDays = 10),
+                    nomisAward(sequence = 103, nomisTypeCode = "ADA", sanctionDays = 8),
+                  ),
+                ),
+              ),
+            ),
+          )
+        }
+      }
+    }
   }
 
-  private fun nomisHearing(hearingId: Long, hearingDateTime: LocalDateTime) = Hearing(
+  private fun nomisHearing(
+    hearingId: Long,
+    hearingDateTime: LocalDateTime,
+    awards: List<HearingResultAward> = emptyList(),
+  ) = Hearing(
     hearingId = hearingId,
-    hearingResults = emptyList(),
     createdDateTime = "2021-03-01T10:00:00",
     createdByUsername = "JTEST_GEN",
     notifications = emptyList(),
@@ -268,30 +398,77 @@ class AdjudicationMappingCreatorTest {
       code = "MDI-1-1-1",
       description = "MDI-1-1-1",
     ),
+    hearingResults = listOf(nomisHearingResult(awards)),
   )
+
+  private fun nomisHearingResult(awards: List<HearingResultAward>): HearingResult {
+    val offence = AdjudicationOffence("51:22", description = "Disobeys any lawful order")
+    return HearingResult(
+      charge = AdjudicationCharge(offence = offence, chargeSequence = 1),
+      offence = offence,
+      resultAwards = awards,
+      createdDateTime = "2021-03-01T10:00:00",
+      createdByUsername = "JTEST_GEN",
+    )
+  }
+
+  private fun nomisAward(
+    sequence: Int,
+    nomisTypeCode: String,
+    sanctionDays: Int? = null,
+    sanctionMonths: Int? = null,
+    effectiveDate: LocalDate = LocalDate.now(),
+    nomisSanctionStatus: String = "IMMEDIATE",
+  ): HearingResultAward {
+    return HearingResultAward(
+      sequence = sequence,
+      adjudicationNumber = 123456,
+      effectiveDate = effectiveDate,
+      chargeSequence = 1,
+      createdByUsername = "JTEST_GEN",
+      sanctionType = CodeDescription(nomisTypeCode, "$nomisTypeCode description"),
+      sanctionDays = sanctionDays,
+      sanctionMonths = sanctionMonths,
+      sanctionStatus = CodeDescription(nomisSanctionStatus, "$nomisSanctionStatus description"),
+    )
+  }
 
   private fun dpsHearing(hearingId: Long, hearingDateTime: LocalDateTime) =
     // language=JSON
     """
     {
-                "id": $hearingId,
-                "locationId": 27187,
-                "dateTimeOfHearing": "$hearingDateTime",
-                "oicHearingType": "GOV_ADULT",
-                "agencyId": "MDI",
-                "outcome": {
-                      "id": 962,
-                      "adjudicator": "JTEST_GEN",
-                      "code": "COMPLETE",
-                      "plea": "GUILTY"
-                  }
-            }
+        "id": $hearingId,
+        "locationId": 27187,
+        "dateTimeOfHearing": "$hearingDateTime",
+        "oicHearingType": "GOV_ADULT",
+        "agencyId": "MDI",
+        "outcome": {
+          "id": 962,
+          "adjudicator": "JTEST_GEN",
+          "code": "COMPLETE",
+          "plea": "GUILTY"
+        }
+    }
+    """.trimIndent()
+
+  private fun dpsPunishment(punishmentId: Long, days: Int, type: PunishmentDto.Type) =
+    // language=JSON
+    """
+    {
+      "id": $punishmentId,
+      "type": "${type.name}",
+      "schedule": {
+          "days": $days
+      },
+      "canRemove": true
+   }
     """.trimIndent()
 
   private fun nomisAdjudication(
     adjudicationNumber: Long,
     chargeSequence: Int,
     hearings: List<Hearing> = emptyList(),
+    bookingId: Long = 12345L,
   ): AdjudicationChargeResponse {
     return AdjudicationChargeResponse(
       adjudicationSequence = chargeSequence,
@@ -336,7 +513,7 @@ class AdjudicationMappingCreatorTest {
         repairs = listOf(),
 
       ),
-      bookingId = 12345L,
+      bookingId = bookingId,
       offenderNo = "A1234KL",
       gender = CodeDescription("F", "Female"),
       partyAddedDate = LocalDate.now(),
