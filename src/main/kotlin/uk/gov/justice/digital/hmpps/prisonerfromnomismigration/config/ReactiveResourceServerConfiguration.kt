e480bd6f131e7459013e9ad196e23884848b8ce6
Mike Halma <mike.halma@digital.justice.gov.uk>
2024-02-08 12:15:32 +0000

    SDIT-1335 Implement hmpps-kotlin-spring-boot-starter lib

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/config/ReactiveResourceServerConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/config/ReactiveResourceServerConfiguration.kt
@@ -2,33 +2,15 @@ package uk.gov.justice.digital.hmpps.prisonerfromnomismigration.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
-import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
-import org.springframework.security.config.web.server.ServerHttpSecurity
-import org.springframework.security.config.web.server.invoke
-import org.springframework.security.web.server.SecurityWebFilterChain
+import uk.gov.justice.hmpps.kotlin.auth.dsl.ResourceServerConfigurationCustomizer
 
 @Configuration
-@EnableWebFluxSecurity
-@EnableReactiveMethodSecurity(useAuthorizationManager = false)
-@EnableR2dbcRepositories
 class ReactiveResourceServerConfiguration {
 
   @Bean
-  fun springSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain =
-    http {
-      // Can't have CSRF protection as requires session
-      csrf { disable() }
-      authorizeExchange {
-        listOf(
-          "/webjars/**", "/favicon.ico", "/csrf",
-          "/health/**", "/info", "/h2-console/**",
-          "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html",
-          "/queue-admin/retry-all-dlqs",
-        ).forEach { authorize(it, permitAll) }
-        authorize(anyExchange, authenticated)
-      }
-      oauth2ResourceServer { jwt { jwtAuthenticationConverter = ReactiveAuthAwareTokenConverter() } }
+  fun resourceServerCustomizer() = ResourceServerConfigurationCustomizer.build {
+    unauthorizedRequestPaths {
+      addPaths = setOf("/queue-admin/retry-all-dlqs")
     }
+  }
 }
