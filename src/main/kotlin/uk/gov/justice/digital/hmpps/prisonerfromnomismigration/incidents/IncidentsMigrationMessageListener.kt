c1edc92ca4b2745b56faa25e892c4c9abb3edcec
karenmillermoj <karen.miller@digital.justice.gov.uk>
2024-02-13 17:21:53 +0000

    SDIT-1375 Reduce Incidents Listener threads to 8

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/incidents/IncidentsMigrationMessageListener.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/incidents/IncidentsMigrationMessageListener.kt
@@ -25,7 +25,7 @@ class IncidentsMigrationMessageListener(
   incidentsMigrationService,
 ) {
 
-  @SqsListener(INCIDENTS_QUEUE_ID, factory = "hmppsQueueContainerFactoryProxy")
+  @SqsListener(INCIDENTS_QUEUE_ID, factory = "hmppsQueueContainerFactoryProxy", maxConcurrentMessages = "8", maxMessagesPerPoll = "8")
   @WithSpan(value = "dps-syscon-migration_incidents_queue", kind = SpanKind.SERVER)
   fun onIncidentMessage(message: String, rawMessage: Message): CompletableFuture<Void>? {
     return onMessage(message, rawMessage)
