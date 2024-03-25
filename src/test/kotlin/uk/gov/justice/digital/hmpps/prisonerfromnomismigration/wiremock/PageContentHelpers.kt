f9b7c8a0357de31f6959ba5c16b365180f5e691f
Andy Marke <andy.marke@digital.justice.gov.uk>
2024-03-25 15:11:53 +0000

    SDIT-1614 Alert migration plumbing
    TODO
    * Retrieve Alert
    * Migrate it

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/wiremock/PageContentHelpers.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/wiremock/PageContentHelpers.kt
@@ -8,43 +8,32 @@ fun pageContent(
   pageNumber: Long,
   totalElements: Long,
   size: Int,
-) =
-  // language=json
-  """
-{
-    "content": [
-        $content
-    ],
-    "pageable": {
-        "sort": {
-            "empty": false,
-            "sorted": true,
-            "unsorted": false
-        },
-        "offset": 0,
-        "pageSize": $pageSize,
-        "pageNumber": $pageNumber,
-        "paged": true,
-        "unpaged": false
-    },
-    "last": false,
-    "totalPages": ${totalElements / pageSize + 1},
-    "totalElements": $totalElements,
-    "size": $pageSize,
-    "number": $pageNumber,
-    "sort": {
-        "empty": false,
-        "sorted": true,
-        "unsorted": false
-    },
-    "first": true,
-    "numberOfElements": $size,
-    "empty": false
-}
-  """.trimIndent()
+) = pageContentWithContent(
+  outerContent = "[$content]",
+  pageSize = pageSize,
+  pageNumber = pageNumber,
+  totalElements = totalElements,
+  size = size,
+)
+
 fun pageContent(
   objectMapper: ObjectMapper,
   content: List<Any>,
+  pageSize: Long,
+  pageNumber: Long,
+  totalElements: Long,
+  size: Int,
+) = pageContentWithContent(
+  outerContent = objectMapper.writeValueAsString(content),
+  pageSize = pageSize,
+  pageNumber = pageNumber,
+  totalElements = totalElements,
+  size = size,
+)
+// language=json
+
+private fun pageContentWithContent(
+  outerContent: String,
   pageSize: Long,
   pageNumber: Long,
   totalElements: Long,
@@ -54,7 +43,7 @@ fun pageContent(
   """
 {
 
-    "content": ${objectMapper.writeValueAsString(content)},
+    "content": $outerContent,
     "pageable": {
         "sort": {
             "empty": false,
