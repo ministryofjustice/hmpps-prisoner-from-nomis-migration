854259300667bcd9283fd62d8e45c2bf061033e3
lbennett-moj <34441186+lbennett-moj@users.noreply.github.com>
2024-03-28 15:17:28 +0000

    sdit-1602 court case create (#548)
    Co-authored-by: lbennett-moj <lucy.bennett@digital .justice.gov.uk>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/courtsentencing/CourtSentencingMappingApiService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/courtsentencing/CourtSentencingMappingApiService.kt
@@ -5,11 +5,12 @@ import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.helpers.awaitBodyOrNullWhenNotFound
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.integration.history.MigrationMapping
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomismappings.model.CourtCaseAllMappingDto
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomismappings.model.CourtCaseMappingDto
 
 @Service
 class CourtSentencingMappingApiService(@Qualifier("mappingApiWebClient") webClient: WebClient) :
-  MigrationMapping<CourtCaseMappingDto>(domainUrl = "/mapping/court-sentencing/court-cases", webClient) {
+  MigrationMapping<CourtCaseAllMappingDto>(domainUrl = "/mapping/court-sentencing/court-cases", webClient) {
   suspend fun getCourtCaseOrNullByNomisId(courtCaseId: Long): CourtCaseMappingDto? = webClient.get()
     .uri(
       "/mapping/court-sentencing/court-cases/nomis-court-case-id/{courtCase}",
