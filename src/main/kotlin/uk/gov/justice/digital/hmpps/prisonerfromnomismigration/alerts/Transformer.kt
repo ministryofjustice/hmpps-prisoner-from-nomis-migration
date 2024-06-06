82442756f05a16a914784530504ff42e7992fc33
andymarke <andy.marke@digital.justice.gov.uk>
2024-06-06 11:40:31 +0100

    SDIT-1475 Refresh alert and nomis alert OpenApi after clean-up

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/alerts/Transformer.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/alerts/Transformer.kt
@@ -3,7 +3,6 @@ package uk.gov.justice.digital.hmpps.prisonerfromnomismigration.alerts
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.alerts.model.CreateAlert
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.alerts.model.MergeAlert
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.alerts.model.MigrateAlert
-import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.alerts.model.MigrateAlertRequest
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.alerts.model.UpdateAlert
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomissync.model.AlertResponse
 import java.time.LocalDateTime
@@ -23,22 +22,6 @@ fun AlertResponse.toDPSUpdateAlert() = UpdateAlert(
   activeTo = this.expiryDate,
   authorisedBy = this.authorisedBy,
   appendComment = null,
-)
-
-fun AlertResponse.toDPSMigratedAlert(offenderNo: String) = MigrateAlertRequest(
-  prisonNumber = offenderNo,
-  alertCode = this.alertCode.code,
-  description = this.comment,
-  activeFrom = this.date,
-  activeTo = this.expiryDate,
-  authorisedBy = this.authorisedBy,
-  createdAt = LocalDateTime.parse(this.audit.createDatetime),
-  createdBy = this.audit.createUsername,
-  createdByDisplayName = this.audit.createDisplayName ?: this.audit.createUsername,
-  updatedAt = this.audit.modifyDatetime?.let { LocalDateTime.parse(this.audit.modifyDatetime) },
-  updatedBy = this.audit.modifyUserId,
-  updatedByDisplayName = this.audit.modifyDisplayName ?: this.audit.modifyUserId,
-  comments = emptyList(),
 )
 
 fun AlertResponse.toDPSMigratedAlert() = MigrateAlert(
