a494be3f1a46eb452a36d4a3daaed31d75407144
steverendell <steve.rendell@syscon.net>
2024-07-01 15:43:27 +0100

    SDIT-1852 Casenotes migration

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/casenotes/CaseNotesApiMockServer.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/casenotes/CaseNotesApiMockServer.kt
@@ -40,7 +40,7 @@ class CaseNotesApiMockServer : WireMockServer(WIREMOCK_PORT) {
 
   fun stubHealthPing(status: Int) {
     stubFor(
-      get("/health/ping").willReturn(
+      get("/mock/health/ping").willReturn(
         aResponse()
           .withHeader("Content-Type", "application/json")
           .withBody(if (status == 200) "pong" else "some error")
