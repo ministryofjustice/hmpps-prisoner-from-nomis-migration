08d98b980788e47e5a16fef52e822ad1667c9941
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2024-07-23 09:56:06 +0100

    SDIT-1825 Add endpoints reuqired for migration UI (#730)

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/prisonperson/PrisonPersonMigrationIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/prisonperson/PrisonPersonMigrationIntTest.kt
@@ -5,11 +5,15 @@ import com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor
 import com.github.tomakehurst.wiremock.client.WireMock.putRequestedFor
 import com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo
 import com.github.tomakehurst.wiremock.client.WireMock.urlMatching
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runTest
 import org.assertj.core.api.Assertions.assertThat
 import org.awaitility.kotlin.atMost
 import org.awaitility.kotlin.await
 import org.awaitility.kotlin.untilAsserted
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.any
@@ -32,9 +36,13 @@ import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomismappings.mod
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomissync.model.BookingPhysicalAttributesResponse
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomissync.model.PhysicalAttributesResponse
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomissync.model.PrisonerPhysicalAttributesResponse
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.persistence.repository.MigrationHistory
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.persistence.repository.MigrationHistoryRepository
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.prisonperson.PrisonPersonDpsApiExtension.Companion.dpsPrisonPersonServer
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.prisonperson.model.PhysicalAttributesMigrationResponse
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.sentencing.MigrationResult
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.service.MigrationStatus
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.service.MigrationType
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.wiremock.MappingApiExtension.Companion.mappingApi
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.wiremock.NomisApiExtension.Companion.nomisApi
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.wiremock.withRequestBodyJsonPath
@@ -51,7 +59,11 @@ class PrisonPersonMigrationIntTest : SqsIntegrationTestBase() {
   @Autowired
   private lateinit var prisonPersonMappingApi: PrisonPersonMappingApiMockServer
 
+  @Autowired
+  private lateinit var migrationHistoryRepository: MigrationHistoryRepository
+
   @Nested
+  @DisplayName("/migrate/prisonperson/physical-attributes")
   inner class MigratePhysicalAttributes {
     private lateinit var migrationResult: MigrationResult
 
@@ -551,5 +563,156 @@ class PrisonPersonMigrationIntTest : SqsIntegrationTestBase() {
         ),
       ),
     )
+  }
+
+  @Nested
+  @DisplayName("/migrate/prisonperson/history")
+  inner class GetAllHistory {
+    @BeforeEach
+    fun createHistoryRecords() {
+      runBlocking {
+        migrationHistoryRepository.save(
+          MigrationHistory(
+            migrationId = "2020-01-01T00:00:00",
+            whenStarted = LocalDateTime.parse("2020-01-01T00:00:00"),
+            whenEnded = LocalDateTime.parse("2020-01-01T01:00:00"),
+            status = MigrationStatus.COMPLETED,
+            estimatedRecordCount = 123_567,
+            filter = "",
+            recordsMigrated = 123_560,
+            recordsFailed = 7,
+            migrationType = MigrationType.PRISONPERSON,
+          ),
+        )
+        migrationHistoryRepository.save(
+          MigrationHistory(
+            migrationId = "2020-01-02T00:00:00",
+            whenStarted = LocalDateTime.parse("2020-01-01T00:00:00"),
+            whenEnded = LocalDateTime.parse("2020-01-01T01:00:00"),
+            status = MigrationStatus.COMPLETED,
+            estimatedRecordCount = 123_567,
+            filter = "",
+            recordsMigrated = 123_560,
+            recordsFailed = 7,
+            migrationType = MigrationType.PRISONPERSON,
+          ),
+        )
+      }
+    }
+
+    @AfterEach
+    fun tearDown() {
+      runBlocking {
+        migrationHistoryRepository.deleteAll()
+      }
+    }
+
+    @Test
+    fun `access forbidden when no role`() {
+      webTestClient.get().uri("/migrate/prisonperson/history")
+        .headers(setAuthorisation(roles = listOf()))
+        .exchange()
+        .expectStatus().isForbidden
+    }
+
+    @Test
+    fun `access forbidden with wrong role`() {
+      webTestClient.get().uri("/migrate/prisonperson/history")
+        .headers(setAuthorisation(roles = listOf("ROLE_BANANAS")))
+        .exchange()
+        .expectStatus().isForbidden
+    }
+
+    @Test
+    fun `access unauthorised with no auth token`() {
+      webTestClient.get().uri("/migrate/prisonperson/history")
+        .header("Content-Type", "application/json")
+        .exchange()
+        .expectStatus().isUnauthorized
+    }
+
+    @Test
+    internal fun `can read all records`() {
+      webTestClient.get().uri("/migrate/prisonperson/history")
+        .headers(setAuthorisation(roles = listOf("ROLE_MIGRATE_PRISONPERSON")))
+        .header("Content-Type", "application/json")
+        .exchange()
+        .expectStatus().isOk
+        .expectBody()
+        .jsonPath("$.size()").isEqualTo(2)
+        .jsonPath("$[0].migrationId").isEqualTo("2020-01-01T00:00:00")
+        .jsonPath("$[1].migrationId").isEqualTo("2020-01-02T00:00:00")
+    }
+  }
+
+  @Nested
+  @DisplayName("/migrate/prisonperson/history/{migrationid}")
+  inner class GetSingleHistory {
+    @BeforeEach
+    internal fun createHistoryRecords() = runTest {
+      migrationHistoryRepository.save(
+        MigrationHistory(
+          migrationId = "2020-01-01T00:00:00",
+          whenStarted = LocalDateTime.parse("2020-01-01T00:00:00"),
+          whenEnded = LocalDateTime.parse("2020-01-01T01:00:00"),
+          status = MigrationStatus.COMPLETED,
+          estimatedRecordCount = 123_567,
+          filter = "",
+          recordsMigrated = 123_560,
+          recordsFailed = 7,
+          migrationType = MigrationType.PRISONPERSON,
+        ),
+      )
+    }
+
+    @AfterEach
+    fun tearDown() = runTest {
+      migrationHistoryRepository.deleteAll()
+    }
+
+    @Test
+    fun `access forbidden when no role`() {
+      webTestClient.get().uri("/migrate/prisonperson/history/2020-01-01T00:00:00")
+        .headers(setAuthorisation(roles = listOf()))
+        .exchange()
+        .expectStatus().isForbidden
+    }
+
+    @Test
+    fun `access forbidden with wrong role`() {
+      webTestClient.get().uri("/migrate/prisonperson/history/2020-01-01T00:00:00")
+        .headers(setAuthorisation(roles = listOf("ROLE_BANANAS")))
+        .exchange()
+        .expectStatus().isForbidden
+    }
+
+    @Test
+    fun `access unauthorised with no auth token`() {
+      webTestClient.get().uri("/migrate/prisonperson/history/2020-01-01T00:00:00")
+        .header("Content-Type", "application/json")
+        .exchange()
+        .expectStatus().isUnauthorized
+    }
+
+    @Test
+    fun ` not found`() {
+      webTestClient.get().uri("/migrate/prisonperson/history/UNKNOWN")
+        .headers(setAuthorisation(roles = listOf("ROLE_MIGRATE_PRISONPERSON")))
+        .header("Content-Type", "application/json")
+        .exchange()
+        .expectStatus().isNotFound
+    }
+
+    @Test
+    internal fun `can get a migration record`() {
+      webTestClient.get().uri("/migrate/prisonperson/history/2020-01-01T00:00:00")
+        .headers(setAuthorisation(roles = listOf("ROLE_MIGRATE_PRISONPERSON")))
+        .header("Content-Type", "application/json")
+        .exchange()
+        .expectStatus().isOk
+        .expectBody()
+        .consumeWith { println(it) }
+        .jsonPath("migrationId").isEqualTo("2020-01-01T00:00:00")
+    }
   }
 }
