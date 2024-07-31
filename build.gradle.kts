3f1ee85dd8d90a168065c1a93a678bbd067bea49
karenmillermoj <karen.miller@digital.justice.gov.uk>
2024-07-31 12:26:59 +0100

    SDIT-1924 Gradle 6.0.1 with jar upgrades

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import org.jlleitschuh.gradle.ktlint.tasks.KtLintFormatTask
 import org.openapitools.generator.gradle.plugin.tasks.GenerateTask
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.2-beta"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.1"
   kotlin("plugin.spring") version "2.0.0"
   id("org.openapi.generator") version "7.6.0"
 }
@@ -41,12 +41,12 @@ dependencies {
   }
   testImplementation("io.swagger.core.v3:swagger-core-jakarta:2.2.22")
 
-  testImplementation("org.wiremock:wiremock-standalone:3.7.0")
-  testImplementation("org.testcontainers:localstack:1.19.8")
-  testImplementation("com.amazonaws:aws-java-sdk-core:1.12.748")
+  testImplementation("org.wiremock:wiremock-standalone:3.9.1")
+  testImplementation("org.testcontainers:localstack:1.20.0")
+  testImplementation("com.amazonaws:aws-java-sdk-core:1.12.766")
   testImplementation("org.awaitility:awaitility-kotlin:4.2.1")
-  testImplementation("org.testcontainers:postgresql:1.19.8")
-  testImplementation("io.mockk:mockk:1.13.11")
+  testImplementation("org.testcontainers:postgresql:1.20.0")
+  testImplementation("io.mockk:mockk:1.13.12")
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
 }
 
