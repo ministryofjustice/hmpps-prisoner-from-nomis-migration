2a886e121059d913ce271778588e9b97ea25e71f
karenmillermoj <karen.miller@digital.justice.gov.uk>
2024-07-31 12:51:46 +0100

    SDIT-1924 Add telemetry and mockk jars

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ dependencies {
   runtimeOnly("org.postgresql:postgresql:42.7.3")
   runtimeOnly("org.flywaydb:flyway-database-postgresql")
 
-  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.4.0")
+  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.6.0")
 
   testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.0.2-beta-4")
   testImplementation("io.swagger.parser.v3:swagger-parser:2.1.22") {
@@ -46,7 +46,7 @@ dependencies {
   testImplementation("com.amazonaws:aws-java-sdk-core:1.12.748")
   testImplementation("org.awaitility:awaitility-kotlin:4.2.1")
   testImplementation("org.testcontainers:postgresql:1.19.8")
-  testImplementation("io.mockk:mockk:1.13.11")
+  testImplementation("io.mockk:mockk:1.13.12")
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
 }
 
