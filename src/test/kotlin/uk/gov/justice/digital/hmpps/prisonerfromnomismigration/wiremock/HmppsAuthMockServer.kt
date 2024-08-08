f6343b26895ec7fb5c39ebf08f65553478c1b118
andymarke <andy.marke@digital.justice.gov.uk>
2024-08-08 16:19:39 +0100

    SDIT-1931 Set token retrieval timeout
    Also added test to expose issue and demonstrate fix. Test can be deleted once fix is incorporated into kotlin template library

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/wiremock/HmppsAuthMockServer.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/wiremock/HmppsAuthMockServer.kt
@@ -1,17 +1,25 @@
 package uk.gov.justice.digital.hmpps.prisonerfromnomismigration.wiremock
 
 import com.github.tomakehurst.wiremock.WireMockServer
-import com.github.tomakehurst.wiremock.client.WireMock
 import com.github.tomakehurst.wiremock.client.WireMock.aResponse
+import com.github.tomakehurst.wiremock.client.WireMock.equalTo
 import com.github.tomakehurst.wiremock.client.WireMock.get
+import com.github.tomakehurst.wiremock.client.WireMock.post
+import com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo
 import com.github.tomakehurst.wiremock.http.HttpHeader
 import com.github.tomakehurst.wiremock.http.HttpHeaders
+import com.github.tomakehurst.wiremock.stubbing.Scenario.STARTED
 import org.junit.jupiter.api.extension.AfterAllCallback
 import org.junit.jupiter.api.extension.BeforeAllCallback
 import org.junit.jupiter.api.extension.BeforeEachCallback
 import org.junit.jupiter.api.extension.ExtensionContext
+import kotlin.io.encoding.Base64
+import kotlin.io.encoding.ExperimentalEncodingApi
 
-class HmppsAuthApiExtension : BeforeAllCallback, AfterAllCallback, BeforeEachCallback {
+class HmppsAuthApiExtension :
+  BeforeAllCallback,
+  AfterAllCallback,
+  BeforeEachCallback {
   companion object {
     @JvmField
     val hmppsAuth = HmppsAuthMockServer()
@@ -39,7 +47,7 @@ class HmppsAuthMockServer : WireMockServer(WIREMOCK_PORT) {
 
   fun stubGrantToken() {
     stubFor(
-      WireMock.post(WireMock.urlEqualTo("/auth/oauth/token"))
+      post(urlEqualTo("/auth/oauth/token"))
         .willReturn(
           aResponse()
             .withHeaders(HttpHeaders(HttpHeader("Content-Type", "application/json")))
@@ -51,6 +59,48 @@ class HmppsAuthMockServer : WireMockServer(WIREMOCK_PORT) {
               """.trimIndent(),
             ),
         ),
+    )
+  }
+
+  @OptIn(ExperimentalEncodingApi::class)
+  fun stubGrantToken(delayMs: Int, timeoutForAuthorizationHeader: String) {
+    val encodedAuthHeader = Base64.encode(timeoutForAuthorizationHeader.toByteArray())
+    stubFor(
+      post(urlEqualTo("/auth/oauth/token"))
+        .inScenario("TokenTimeoutScenario")
+        .whenScenarioStateIs("Grant Timeout")
+        .withHeader("Authorization", equalTo("Basic $encodedAuthHeader"))
+        .willReturn(
+          aResponse()
+            .withHeaders(HttpHeaders(HttpHeader("Content-Type", "application/json")))
+            .withBody(
+              """{
+                    "token_type": "bearer",
+                    "access_token": "ABCDE"
+                }
+              """.trimIndent(),
+            ).withFixedDelay(delayMs),
+        )
+        .willSetStateTo("Grant Success"),
+    )
+
+    stubFor(
+      post(urlEqualTo("/auth/oauth/token"))
+        .inScenario("TokenTimeoutScenario")
+        .whenScenarioStateIs("Grant Success")
+        .withHeader("Authorization", equalTo("Basic $encodedAuthHeader"))
+        .willReturn(
+          aResponse()
+            .withHeaders(HttpHeaders(HttpHeader("Content-Type", "application/json")))
+            .withBody(
+              """{
+                    "token_type": "bearer",
+                    "access_token": "ABCDE"
+                }
+              """.trimIndent(),
+            ),
+        )
+        .willSetStateTo(STARTED),
     )
   }
 
