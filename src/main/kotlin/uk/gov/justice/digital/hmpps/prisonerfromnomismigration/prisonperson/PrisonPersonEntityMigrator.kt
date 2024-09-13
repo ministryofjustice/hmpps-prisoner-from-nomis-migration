30d2201cfceb91aa43784c2bd85ac8b7b36a7bab
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2024-09-13 11:14:49 +0100

    SDIT-2021 Implement profile detail physical attributes migration and add basic test (#811)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/prisonperson/PrisonPersonEntityMigrator.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/prisonperson/PrisonPersonEntityMigrator.kt
@@ -3,6 +3,8 @@ package uk.gov.justice.digital.hmpps.prisonerfromnomismigration.prisonperson
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomismappings.model.PrisonPersonMigrationMappingRequest
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomismappings.model.PrisonPersonMigrationMappingRequest.MigrationType.PHYSICAL_ATTRIBUTES
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomismappings.model.PrisonPersonMigrationMappingRequest.MigrationType.PROFILE_DETAILS_PHYSICAL_ATTRIBUTES
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.prisonperson.profiledetails.ProfileDetailPhysicalAttributesEntityMigrator
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.prisonperson.physicalattributes.PhysicalAttributesEntityMigrator as PhysicalAttributesMigrator
 
 interface PrisonPersonEntityMigrator {
@@ -16,6 +18,7 @@ class PrisonPersonEntityMigratorService(
   fun migrator(migrationType: PrisonPersonMigrationMappingRequest.MigrationType): PrisonPersonEntityMigrator =
     when (migrationType) {
       PHYSICAL_ATTRIBUTES -> entityMigrators.find { it is PhysicalAttributesMigrator }
+      PROFILE_DETAILS_PHYSICAL_ATTRIBUTES -> entityMigrators.find { it is ProfileDetailPhysicalAttributesEntityMigrator }
     }
       ?: error("No Entity Migrator found for migrationType=$migrationType")
 }
