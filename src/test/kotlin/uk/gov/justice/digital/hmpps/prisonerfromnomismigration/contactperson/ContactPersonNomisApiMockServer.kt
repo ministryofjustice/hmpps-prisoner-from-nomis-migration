349b8ae18c1a37fe6acca6635f230a0da92c8da3
andymarke <andy.marke@digital.justice.gov.uk>
2024-10-02 16:56:02 +0100

    SDIT-2081 Contact person nomis api integration

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/contactperson/ContactPersonNomisApiMockServer.kt
@@ -0,0 +1,93 @@
+package uk.gov.justice.digital.hmpps.prisonerfromnomismigration.contactperson
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.github.tomakehurst.wiremock.client.WireMock.aResponse
+import com.github.tomakehurst.wiremock.client.WireMock.get
+import com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo
+import com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo
+import com.github.tomakehurst.wiremock.matching.RequestPatternBuilder
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Component
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomismappings.model.ErrorResponse
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomissync.model.ContactPerson
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomissync.model.NomisAudit
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomissync.model.PagePersonIdResponse
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomissync.model.PersonIdResponse
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.wiremock.NomisApiExtension.Companion.nomisApi
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+@Component
+class ContactPersonNomisApiMockServer(private val objectMapper: ObjectMapper) {
+  fun stubGetPerson(
+    personId: Long = 123456,
+    person: ContactPerson = contactPerson(),
+  ) {
+    nomisApi.stubFor(
+      get(urlEqualTo("/persons/$personId")).willReturn(
+        aResponse()
+          .withHeader("Content-Type", "application/json")
+          .withStatus(HttpStatus.OK.value())
+          .withBody(objectMapper.writeValueAsString(person)),
+      ),
+    )
+  }
+
+  fun stubGetPerson(
+    personId: Long = 123456,
+    status: HttpStatus,
+    error: ErrorResponse = ErrorResponse(status = status.value()),
+  ) {
+    nomisApi.stubFor(
+      get(urlEqualTo("/persons/$personId")).willReturn(
+        aResponse()
+          .withHeader("Content-Type", "application/json")
+          .withStatus(status.value())
+          .withBody(objectMapper.writeValueAsString(error)),
+      ),
+    )
+  }
+
+  fun stubGetPersonIdsToMigrate(
+    personIdPages: PagePersonIdResponse = pagePersonIdResponse(),
+  ) {
+    nomisApi.stubFor(
+      get(urlPathEqualTo("/persons/ids")).willReturn(
+        aResponse()
+          .withHeader("Content-Type", "application/json")
+          .withStatus(HttpStatus.OK.value())
+          .withBody(objectMapper.writeValueAsString(personIdPages)),
+      ),
+    )
+  }
+
+  fun verify(pattern: RequestPatternBuilder) = nomisApi.verify(pattern)
+  fun verify(count: Int, pattern: RequestPatternBuilder) = nomisApi.verify(count, pattern)
+}
+
+fun contactPerson(): ContactPerson = ContactPerson(
+  personId = 123456,
+  firstName = "KWAME",
+  lastName = "KOBE",
+  interpreterRequired = false,
+  keepBiometrics = false,
+  audit = NomisAudit(
+    createDatetime = LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME),
+    createUsername = "Q1251T",
+  ),
+  phoneNumbers = emptyList(),
+  addresses = emptyList(),
+  emailAddresses = emptyList(),
+  employments = emptyList(),
+  identifiers = emptyList(),
+  contacts = emptyList(),
+  restrictions = emptyList(),
+)
+
+fun pagePersonIdResponse(): PagePersonIdResponse = PagePersonIdResponse(
+  totalElements = 1,
+  totalPages = 1,
+  number = 0,
+  numberOfElements = 1,
+  content = listOf(PersonIdResponse(123456)),
+)
---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/contactperson/ContactPersonNomisApiServiceTest.kt
@@ -0,0 +1,129 @@
+package uk.gov.justice.digital.hmpps.prisonerfromnomismigration.contactperson
+
+import com.github.tomakehurst.wiremock.client.WireMock.anyUrl
+import com.github.tomakehurst.wiremock.client.WireMock.equalTo
+import com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor
+import com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo
+import kotlinx.coroutines.test.runTest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Import
+import org.springframework.http.HttpStatus
+import org.springframework.web.reactive.function.client.WebClientResponseException
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.helper.SpringAPIServiceTest
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomissync.model.PersonIdResponse
+import java.time.LocalDate
+
+@SpringAPIServiceTest
+@Import(ContactPersonNomisApiService::class, ContactPersonNomisApiMockServer::class)
+class ContactPersonNomisApiServiceTest {
+  @Autowired
+  private lateinit var apiService: ContactPersonNomisApiService
+
+  @Autowired
+  private lateinit var mockServer: ContactPersonNomisApiMockServer
+
+  @Nested
+  inner class GetPerson {
+    @Test
+    fun `will pass oath2 token to service`() = runTest {
+      mockServer.stubGetPerson(personId = 1234567)
+
+      apiService.getPerson(nomisPersonId = 1234567)
+
+      mockServer.verify(
+        getRequestedFor(anyUrl()).withHeader("Authorization", equalTo("Bearer ABCDE")),
+      )
+    }
+
+    @Test
+    fun `will pass NOMIS id to service`() = runTest {
+      mockServer.stubGetPerson(personId = 1234567)
+
+      apiService.getPerson(nomisPersonId = 1234567)
+
+      mockServer.verify(
+        getRequestedFor(urlPathEqualTo("/persons/1234567")),
+      )
+    }
+
+    @Test
+    fun `will return person`() = runTest {
+      mockServer.stubGetPerson(personId = 1234567, person = contactPerson().copy(lastName = "Smith"))
+
+      val person = apiService.getPerson(nomisPersonId = 1234567)
+
+      assertThat(person.lastName).isEqualTo("Smith")
+    }
+
+    @Test
+    fun `will throw error when person does not exist`() = runTest {
+      mockServer.stubGetPerson(personId = 1234567, status = HttpStatus.NOT_FOUND)
+
+      assertThrows<WebClientResponseException.NotFound> {
+        apiService.getPerson(nomisPersonId = 1234567)
+      }
+    }
+  }
+
+  @Nested
+  inner class GetPersonIdsToMigrate {
+    @Test
+    fun `will pass oath2 token to service`() = runTest {
+      mockServer.stubGetPersonIdsToMigrate()
+
+      apiService.getPersonIdsToMigrate()
+
+      mockServer.verify(
+        getRequestedFor(anyUrl()).withHeader("Authorization", equalTo("Bearer ABCDE")),
+      )
+    }
+
+    @Test
+    fun `will pass mandatory parameters to service`() = runTest {
+      mockServer.stubGetPersonIdsToMigrate()
+
+      apiService.getPersonIdsToMigrate(pageNumber = 12, pageSize = 20)
+
+      mockServer.verify(
+        getRequestedFor(anyUrl())
+          .withQueryParam("fromDate", equalTo(""))
+          .withQueryParam("toDate", equalTo(""))
+          .withQueryParam("page", equalTo("12"))
+          .withQueryParam("size", equalTo("20")),
+      )
+    }
+
+    @Test
+    fun `can pass optional parameters to service`() = runTest {
+      mockServer.stubGetPersonIdsToMigrate()
+
+      apiService.getPersonIdsToMigrate(
+        fromDate = LocalDate.parse("2020-01-01"),
+        toDate = LocalDate.parse("2020-01-02"),
+        pageNumber = 12,
+        pageSize = 20,
+      )
+
+      mockServer.verify(
+        getRequestedFor(anyUrl())
+          .withQueryParam("fromDate", equalTo("2020-01-01"))
+          .withQueryParam("toDate", equalTo("2020-01-02")),
+      )
+    }
+
+    @Test
+    fun `will return person ids`() = runTest {
+      mockServer.stubGetPersonIdsToMigrate(personIdPages = pagePersonIdResponse().copy(content = listOf(PersonIdResponse(1234567), PersonIdResponse(1234568))))
+
+      val pages = apiService.getPersonIdsToMigrate(pageNumber = 12, pageSize = 20)
+
+      assertThat(pages.content).hasSize(2)
+      assertThat(pages.content!![0].personId).isEqualTo(1234567)
+      assertThat(pages.content!![1].personId).isEqualTo(1234568)
+    }
+  }
+}
