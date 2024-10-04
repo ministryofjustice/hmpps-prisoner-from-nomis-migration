485316d3d698be4c35fe4fd9150eaf2dedd9cedf
petergphillips <5099053+petergphillips@users.noreply.github.com>
2024-10-04 16:50:27 +0100

    SDIT-2060: 🐛 Ensure that MessageAttributes don't get added to payload if null (#860)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/listeners/ListenerHelpers.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/listeners/ListenerHelpers.kt
@@ -1,19 +1,19 @@
+@file:Suppress("PropertyName")
+
 package uk.gov.justice.digital.hmpps.prisonerfromnomismigration.listeners
 
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL
 import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.databind.annotation.JsonNaming
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.data.MigrationContext
-import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.data.SynchronisationContext
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.service.MigrationMessage
-import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.service.SynchronisationMessage
 
 inline fun <reified T> migrationContext(message: MigrationMessage<*, T>): MigrationContext<T> =
   message.context
 
-inline fun <reified T> synchronisationContext(message: SynchronisationMessage<*, T>): SynchronisationContext<T> =
-  message.context
-
 @JsonNaming(value = PropertyNamingStrategies.UpperCamelCaseStrategy::class)
+@JsonInclude(NON_NULL)
 data class SQSMessage(val Type: String, val Message: String, val MessageId: String? = null, val MessageAttributes: MessageAttributes? = null)
 data class MessageAttributes(val eventType: EventType)
 data class EventType(val Value: String, val Type: String)
