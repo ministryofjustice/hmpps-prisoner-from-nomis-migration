b0260a6a85aa070dab913c84af263491ddab9a1c
andymarke <andy.marke@digital.justice.gov.uk>
2024-10-14 13:09:54 +0100

    SDIT-2113 Contact Person events listener

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/contactperson/ContactPersonEventListener.kt
@@ -0,0 +1,44 @@
+package uk.gov.justice.digital.hmpps.prisonerfromnomismigration.contactperson
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import io.awspring.cloud.sqs.annotation.SqsListener
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.listeners.EventFeatureSwitch
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.listeners.SQSMessage
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.listeners.asCompletableFuture
+import java.util.concurrent.CompletableFuture
+
+@Service
+class ContactPersonEventListener(
+  private val objectMapper: ObjectMapper,
+  private val eventFeatureSwitch: EventFeatureSwitch,
+) {
+
+  private companion object {
+    val log: Logger = LoggerFactory.getLogger(this::class.java)
+  }
+
+  @SqsListener("eventcontactperson", factory = "hmppsQueueContainerFactoryProxy")
+  fun onMessage(message: String): CompletableFuture<Void> {
+    log.debug("Received offender event message {}", message)
+    val sqsMessage: SQSMessage = objectMapper.readValue(message)
+    return asCompletableFuture {
+      when (sqsMessage.Type) {
+        "Notification" -> {
+          val eventType = sqsMessage.MessageAttributes!!.eventType.Value
+          if (eventFeatureSwitch.isEnabled(eventType, "contactperson")) {
+            @Suppress("UNUSED_EXPRESSION")
+            when (eventType) {
+              else -> log.info("Received a message I wasn't expecting {}", eventType)
+            }
+          } else {
+            log.info("Feature switch is disabled for event {}", eventType)
+          }
+        }
+      }
+    }
+  }
+}
