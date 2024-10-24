090512e25271cda4a19a3f0336663445e4e6dd94
Andy Marke <andy.marke@digital.justice.gov.uk>
2024-10-24 16:30:38 +0100

    SDIT-2070 Basic integration to person identifier crud events (#902)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/contactperson/ContactPersonEventListener.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/contactperson/ContactPersonEventListener.kt
@@ -54,6 +54,9 @@ class ContactPersonEventListener(
               "PERSON_EMPLOYMENTS-INSERTED" -> service.personEmploymentAdded(sqsMessage.Message.fromJson())
               "PERSON_EMPLOYMENTS-UPDATED" -> service.personEmploymentUpdated(sqsMessage.Message.fromJson())
               "PERSON_EMPLOYMENTS-DELETED" -> service.personEmploymentDeleted(sqsMessage.Message.fromJson())
+              "PERSON_IDENTIFIERS-INSERTED" -> service.personIdentifierAdded(sqsMessage.Message.fromJson())
+              "PERSON_IDENTIFIERS-UPDATED" -> service.personIdentifierUpdated(sqsMessage.Message.fromJson())
+              "PERSON_IDENTIFIERS-DELETED" -> service.personIdentifierDeleted(sqsMessage.Message.fromJson())
               else -> log.info("Received a message I wasn't expecting {}", eventType)
             }
           } else {
@@ -116,5 +119,11 @@ data class PersonInternetAddressEvent(
 data class PersonEmploymentEvent(
   val personId: Long,
   val employmentSequence: Long,
+  val auditModuleName: String,
+)
+
+data class PersonIdentifierEvent(
+  val personId: Long,
+  val identifierSequence: Long,
   val auditModuleName: String,
 )
