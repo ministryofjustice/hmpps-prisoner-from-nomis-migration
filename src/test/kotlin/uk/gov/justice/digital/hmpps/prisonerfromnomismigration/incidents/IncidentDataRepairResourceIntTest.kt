84ceb09f5e9a27f181bc7de1a33786798472886f
karenmillermoj <karen.miller@digital.justice.gov.uk>
2024-11-08 14:45:18 +0000

    SDIT-2199 Remove annoying wildcard in import

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/incidents/IncidentDataRepairResourceIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/incidents/IncidentDataRepairResourceIntTest.kt
@@ -17,7 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.incidents.IncidentsApiExtension.Companion.incidentsApi
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.integration.SqsIntegrationTestBase
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.wiremock.withRequestBodyJsonPath
-import java.util.*
+import java.util.UUID
 
 class IncidentDataRepairResourceIntTest : SqsIntegrationTestBase() {
   @Autowired
