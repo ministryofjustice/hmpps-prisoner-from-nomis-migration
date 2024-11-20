de168e2e30994c092078180afef12b47dd87d7ba
Andy Marke <andy.marke@digital.justice.gov.uk>
2024-11-20 16:55:04 +0000

    SDIT-2266 Use latest DPS DTOs (#955)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/contactperson/ContactPersonDpsApiService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/contactperson/ContactPersonDpsApiService.kt
@@ -3,12 +3,12 @@ package uk.gov.justice.digital.hmpps.prisonerfromnomismigration.contactperson
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
-import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.contactperson.model.Contact
-import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.contactperson.model.CreatePrisonerContactRequest
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.contactperson.model.MigrateContactRequest
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.contactperson.model.MigrateContactResponse
-import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.contactperson.model.PrisonerContact
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.contactperson.model.SyncContact
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.contactperson.model.SyncCreateContactRequest
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.contactperson.model.SyncCreatePrisonerContactRequest
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.contactperson.model.SyncPrisonerContact
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.helpers.awaitBodyOrLogAndRethrowBadRequest
 
 @Service
@@ -19,13 +19,13 @@ class ContactPersonDpsApiService(@Qualifier("contactPersonApiWebClient") private
     .retrieve()
     .awaitBodyOrLogAndRethrowBadRequest()
 
-  suspend fun createContact(contact: SyncCreateContactRequest): Contact = webClient.post()
+  suspend fun createContact(contact: SyncCreateContactRequest): SyncContact = webClient.post()
     .uri("/sync/contact")
     .bodyValue(contact)
     .retrieve()
     .awaitBodyOrLogAndRethrowBadRequest()
 
-  suspend fun createPrisonerContact(prisonerContact: CreatePrisonerContactRequest): PrisonerContact = webClient.post()
+  suspend fun createPrisonerContact(prisonerContact: SyncCreatePrisonerContactRequest): SyncPrisonerContact = webClient.post()
     .uri("/sync/prisoner-contact")
     .bodyValue(prisonerContact)
     .retrieve()
