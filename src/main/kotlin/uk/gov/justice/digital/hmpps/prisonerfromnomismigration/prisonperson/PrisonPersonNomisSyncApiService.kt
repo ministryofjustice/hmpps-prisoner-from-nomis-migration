839ad6008c962d975406c3bc56270769b0234428
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2024-11-21 10:58:43 +0000

    SDIT-2200 Add client to sync physical attributes from DPS to NOMIS (#956)

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/prisonperson/PrisonPersonNomisSyncApiService.kt
@@ -0,0 +1,30 @@
+package uk.gov.justice.digital.hmpps.prisonerfromnomismigration.prisonperson
+
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.awaitBodilessEntity
+
+class PrisonPersonNomisSyncApiService(@Qualifier("nomisSyncApiWebClient") private val webClient: WebClient) {
+  suspend fun syncPhysicalAttributes(prisonerNumber: String) =
+    webClient.put()
+      .uri {
+        it.path("/prisonperson/{prisonerNumber}/physical-attributes")
+          // Note that we use the DPS field codes here because the nomis-update service transforms them to NOMIS codes
+          .queryParam("fields", PHYSICAL_ATTRIBUTES_DPS_FIELDS)
+          .build(prisonerNumber)
+      }
+      .retrieve()
+      .awaitBodilessEntity()
+}
+
+private val PHYSICAL_ATTRIBUTES_DPS_FIELDS = listOf(
+  "HEIGHT",
+  "WEIGHT",
+  "BUILD",
+  "FACE",
+  "FACIAL_HAIR",
+  "HAIR",
+  "LEFT_EYE_COLOUR",
+  "RIGHT_EYE_COLOUR",
+  "SHOE_SIZE",
+)
