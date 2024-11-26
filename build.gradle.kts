219ed4079d8cc0a3ed3c59dc5fbe29fdeca316c1
petergphillips <5099053+petergphillips@users.noreply.github.com>
2024-11-26 10:41:55 +0000

    SDIT-2318: ⬆️  Upgrade spring boot to 3.4.0 (#963)

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import org.jlleitschuh.gradle.ktlint.tasks.KtLintFormatTask
 import org.openapitools.generator.gradle.plugin.tasks.GenerateTask
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.9"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.1.0"
   kotlin("plugin.spring") version "2.0.21"
   id("org.openapi.generator") version "7.10.0"
 }
@@ -17,12 +17,12 @@ configurations {
 }
 
 dependencies {
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.0.8")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.1.0")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.1.0")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.2.0")
   implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
 
-  implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.6.0")
+  implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.7.0")
 
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8")
@@ -32,21 +32,21 @@ dependencies {
   runtimeOnly("org.springframework.boot:spring-boot-starter-jdbc")
   runtimeOnly("org.postgresql:postgresql:42.7.4")
   runtimeOnly("org.flywaydb:flyway-database-postgresql")
-  implementation("io.opentelemetry:opentelemetry-extension-kotlin:1.44.1")
+  implementation("io.opentelemetry:opentelemetry-extension-kotlin:1.43.0")
 
   implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.9.0")
 
-  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.0.8")
+  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.1.0")
   testImplementation("io.swagger.parser.v3:swagger-parser:2.1.24") {
     exclude(group = "io.swagger.core.v3")
   }
   testImplementation("io.swagger.core.v3:swagger-core-jakarta:2.2.26")
 
   testImplementation("org.wiremock:wiremock-standalone:3.9.2")
-  testImplementation("org.testcontainers:localstack:1.20.3")
+  testImplementation("org.testcontainers:localstack:1.20.4")
   testImplementation("com.amazonaws:aws-java-sdk-core:1.12.778")
   testImplementation("org.awaitility:awaitility-kotlin:4.2.2")
-  testImplementation("org.testcontainers:postgresql:1.20.3")
+  testImplementation("org.testcontainers:postgresql:1.20.4")
   testImplementation("io.mockk:mockk:1.13.13")
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
 }
