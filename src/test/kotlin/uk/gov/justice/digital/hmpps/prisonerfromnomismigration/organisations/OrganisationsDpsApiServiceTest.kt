6c0eae8a7bcca272a3fcd30b72c65587c5ccc1a7
Andy Marke <andy.marke@digital.justice.gov.uk>
2025-02-11 12:29:33 +0000

    SDIT-2447 Integration for Organisation CRUD (#1093)
    TODO - process actual events

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/organisations/OrganisationsDpsApiServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/organisations/OrganisationsDpsApiServiceTest.kt
@@ -1,8 +1,10 @@
 package uk.gov.justice.digital.hmpps.prisonerfromnomismigration.organisations
 
 import com.github.tomakehurst.wiremock.client.WireMock.anyUrl
+import com.github.tomakehurst.wiremock.client.WireMock.deleteRequestedFor
 import com.github.tomakehurst.wiremock.client.WireMock.equalTo
 import com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor
+import com.github.tomakehurst.wiremock.client.WireMock.putRequestedFor
 import com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Nested
@@ -12,6 +14,8 @@ import org.springframework.context.annotation.Import
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.helper.SpringAPIServiceTest
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.organisations.OrganisationsDpsApiExtension.Companion.dpsOrganisationsServer
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.organisations.OrganisationsDpsApiMockServer.Companion.migrateOrganisationRequest
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.organisations.OrganisationsDpsApiMockServer.Companion.syncCreateOrganisationRequest
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.organisations.OrganisationsDpsApiMockServer.Companion.syncUpdateOrganisationRequest
 
 @SpringAPIServiceTest
 @Import(OrganisationsDpsApiService::class, OrganisationsConfiguration::class, OrganisationsDpsApiMockServer::class)
@@ -20,9 +24,9 @@ class OrganisationsDpsApiServiceTest {
   private lateinit var apiService: OrganisationsDpsApiService
 
   @Nested
-  inner class MigrateContact {
+  inner class MigrateOrganisation {
     @Test
-    internal fun `will pass oath2 token to contact endpoint`() = runTest {
+    internal fun `will pass oath2 token to organisation endpoint`() = runTest {
       dpsOrganisationsServer.stubMigrateOrganisation()
 
       apiService.migrateOrganisation(migrateOrganisationRequest())
@@ -41,6 +45,84 @@ class OrganisationsDpsApiServiceTest {
 
       dpsOrganisationsServer.verify(
         postRequestedFor(urlPathEqualTo("/migrate/organisation")),
+      )
+    }
+  }
+
+  @Nested
+  inner class CreateOrganisation {
+    @Test
+    internal fun `will pass oath2 token to organisation endpoint`() = runTest {
+      dpsOrganisationsServer.stubCreateOrganisation()
+
+      apiService.createOrganisation(syncCreateOrganisationRequest())
+
+      dpsOrganisationsServer.verify(
+        postRequestedFor(anyUrl())
+          .withHeader("Authorization", equalTo("Bearer ABCDE")),
+      )
+    }
+
+    @Test
+    fun `will call the POST endpoint`() = runTest {
+      dpsOrganisationsServer.stubCreateOrganisation()
+
+      apiService.createOrganisation(syncCreateOrganisationRequest())
+
+      dpsOrganisationsServer.verify(
+        postRequestedFor(urlPathEqualTo("/sync/organisation")),
+      )
+    }
+  }
+
+  @Nested
+  inner class UpdateOrganisation {
+    @Test
+    internal fun `will pass oath2 token to organisation endpoint`() = runTest {
+      dpsOrganisationsServer.stubUpdateOrganisation(12345)
+
+      apiService.updateOrganisation(12345, syncUpdateOrganisationRequest())
+
+      dpsOrganisationsServer.verify(
+        putRequestedFor(anyUrl())
+          .withHeader("Authorization", equalTo("Bearer ABCDE")),
+      )
+    }
+
+    @Test
+    fun `will call the PUT endpoint`() = runTest {
+      dpsOrganisationsServer.stubUpdateOrganisation(12345)
+
+      apiService.updateOrganisation(12345, syncUpdateOrganisationRequest())
+
+      dpsOrganisationsServer.verify(
+        putRequestedFor(urlPathEqualTo("/sync/organisation/12345")),
+      )
+    }
+  }
+
+  @Nested
+  inner class DeleteOrganisation {
+    @Test
+    internal fun `will pass oath2 token to organisation endpoint`() = runTest {
+      dpsOrganisationsServer.stubDeleteOrganisation(12345)
+
+      apiService.deleteOrganisation(12345)
+
+      dpsOrganisationsServer.verify(
+        deleteRequestedFor(anyUrl())
+          .withHeader("Authorization", equalTo("Bearer ABCDE")),
+      )
+    }
+
+    @Test
+    fun `will call the DELETE endpoint`() = runTest {
+      dpsOrganisationsServer.stubDeleteOrganisation(12345)
+
+      apiService.deleteOrganisation(12345)
+
+      dpsOrganisationsServer.verify(
+        deleteRequestedFor(urlPathEqualTo("/sync/organisation/12345")),
       )
     }
   }
