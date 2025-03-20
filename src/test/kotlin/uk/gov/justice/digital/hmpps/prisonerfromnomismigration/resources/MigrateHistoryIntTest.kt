52ada98974f9794756152bb79a3c15198e4979ca
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2025-03-20 16:28:10 +0000

    SDIT-2609: ✨ Add generic history endpoints

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/resources/MigrateHistoryIntTest.kt
@@ -0,0 +1,317 @@
+package uk.gov.justice.digital.hmpps.prisonerfromnomismigration.resources
+
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.integration.SqsIntegrationTestBase
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.persistence.repository.MigrationHistory
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.persistence.repository.MigrationHistoryRepository
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.service.MigrationStatus
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.service.MigrationType
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.service.MigrationType.VISIT_BALANCE
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.visitbalances.VisitBalanceMappingApiMockServer
+import java.time.LocalDateTime
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class MigrateHistoryIntTest : SqsIntegrationTestBase() {
+  @Autowired
+  private lateinit var mappingApiMock: VisitBalanceMappingApiMockServer
+
+  @Autowired
+  private lateinit var migrationHistoryRepository: MigrationHistoryRepository
+
+  @Nested
+  @DisplayName("GET /migrate/history/all/{migrationType}")
+  inner class GetAll {
+    @BeforeEach
+    internal fun createHistoryRecords() {
+      runBlocking {
+        migrationHistoryRepository.deleteAll()
+        migrationHistoryRepository.save(
+          MigrationHistory(
+            migrationId = "2020-01-01T00:00:00",
+            whenStarted = LocalDateTime.parse("2020-01-01T00:00:00"),
+            whenEnded = LocalDateTime.parse("2020-01-01T01:00:00"),
+            status = MigrationStatus.COMPLETED,
+            estimatedRecordCount = 123_567,
+            filter = "",
+            recordsMigrated = 123_560,
+            recordsFailed = 7,
+            migrationType = VISIT_BALANCE,
+          ),
+        )
+        migrationHistoryRepository.save(
+          MigrationHistory(
+            migrationId = "2020-01-02T00:00:00",
+            whenStarted = LocalDateTime.parse("2020-01-02T00:00:00"),
+            whenEnded = LocalDateTime.parse("2020-01-02T01:00:00"),
+            status = MigrationStatus.COMPLETED,
+            estimatedRecordCount = 123_567,
+            filter = "",
+            recordsMigrated = 123_567,
+            recordsFailed = 0,
+            migrationType = VISIT_BALANCE,
+          ),
+        )
+        migrationHistoryRepository.save(
+          MigrationHistory(
+            migrationId = "2020-01-02T02:00:00",
+            whenStarted = LocalDateTime.parse("2020-01-02T02:00:00"),
+            whenEnded = LocalDateTime.parse("2020-01-02T03:00:00"),
+            status = MigrationStatus.COMPLETED,
+            estimatedRecordCount = 123_567,
+            filter = "",
+            recordsMigrated = 123_567,
+            recordsFailed = 0,
+            migrationType = VISIT_BALANCE,
+          ),
+        )
+        // record of a different type
+        migrationHistoryRepository.save(
+          MigrationHistory(
+            migrationId = "2020-01-03T02:00:00",
+            whenStarted = LocalDateTime.parse("2020-01-03T02:00:00"),
+            whenEnded = LocalDateTime.parse("2020-01-03T03:00:00"),
+            status = MigrationStatus.COMPLETED,
+            estimatedRecordCount = 123_567,
+            filter = "",
+            recordsMigrated = 123_560,
+            recordsFailed = 7,
+            migrationType = MigrationType.ACTIVITIES,
+          ),
+        )
+      }
+    }
+
+    @AfterEach
+    internal fun deleteHistoryRecords() {
+      runBlocking {
+        migrationHistoryRepository.deleteAll()
+      }
+    }
+
+    @Nested
+    inner class Security {
+      @Test
+      fun `access forbidden when no role`() {
+        webTestClient.get().uri("/migrate/history/all/{migrationType}", VISIT_BALANCE)
+          .headers(setAuthorisation(roles = listOf()))
+          .exchange()
+          .expectStatus().isForbidden
+      }
+
+      @Test
+      fun `access forbidden with wrong role`() {
+        webTestClient.get().uri("/migrate/history/all/{migrationType}", VISIT_BALANCE)
+          .headers(setAuthorisation(roles = listOf("BANANAS")))
+          .exchange()
+          .expectStatus().isForbidden
+      }
+
+      @Test
+      fun `access unauthorised with no auth token`() {
+        webTestClient.get().uri("/migrate/history/all/{migrationType}", VISIT_BALANCE)
+          .header("Content-Type", "application/json")
+          .exchange()
+          .expectStatus().isUnauthorized
+      }
+    }
+
+    @Test
+    internal fun `can read all records of the specified type`() {
+      webTestClient.get().uri("/migrate/history/all/{migrationType}", VISIT_BALANCE)
+        .headers(setAuthorisation(roles = listOf("PRISONER_FROM_NOMIS__MIGRATION__RW")))
+        .header("Content-Type", "application/json")
+        .exchange()
+        .expectStatus().isOk
+        .expectBody()
+        .jsonPath("$.size()").isEqualTo(3)
+        .jsonPath("$[0].migrationId").isEqualTo("2020-01-02T02:00:00")
+        .jsonPath("$[1].migrationId").isEqualTo("2020-01-02T00:00:00")
+        .jsonPath("$[2].migrationId").isEqualTo("2020-01-01T00:00:00")
+    }
+  }
+
+  @Nested
+  @DisplayName("GET /migrate/history/{migrationId}")
+  inner class GetSingleHistoryItem {
+    @BeforeEach
+    internal fun createHistoryRecords() {
+      runBlocking {
+        migrationHistoryRepository.deleteAll()
+        migrationHistoryRepository.save(
+          MigrationHistory(
+            migrationId = "2020-01-01T00:00:00",
+            whenStarted = LocalDateTime.parse("2020-01-01T00:00:00"),
+            whenEnded = LocalDateTime.parse("2020-01-01T01:00:00"),
+            status = MigrationStatus.COMPLETED,
+            estimatedRecordCount = 123_567,
+            filter = "",
+            recordsMigrated = 123_560,
+            recordsFailed = 7,
+            migrationType = VISIT_BALANCE,
+          ),
+        )
+      }
+    }
+
+    @AfterEach
+    internal fun deleteHistoryRecords() {
+      runBlocking {
+        migrationHistoryRepository.deleteAll()
+      }
+    }
+
+    @Nested
+    inner class Security {
+      @Test
+      fun `access forbidden when no role`() {
+        webTestClient.get().uri("/migrate/history/2020-01-01T00:00:00")
+          .headers(setAuthorisation(roles = listOf()))
+          .exchange()
+          .expectStatus().isForbidden
+      }
+
+      @Test
+      fun `access forbidden with wrong role`() {
+        webTestClient.get().uri("/migrate/history/2020-01-01T00:00:00")
+          .headers(setAuthorisation(roles = listOf("BANANAS")))
+          .exchange()
+          .expectStatus().isForbidden
+      }
+
+      @Test
+      fun `access unauthorised with no auth token`() {
+        webTestClient.get().uri("/migrate/history/2020-01-01T00:00:00")
+          .header("Content-Type", "application/json")
+          .exchange()
+          .expectStatus().isUnauthorized
+      }
+    }
+
+    @Test
+    internal fun `can read record`() {
+      webTestClient.get().uri("/migrate/history/2020-01-01T00:00:00")
+        .headers(setAuthorisation(roles = listOf("PRISONER_FROM_NOMIS__MIGRATION__RW")))
+        .header("Content-Type", "application/json")
+        .exchange()
+        .expectStatus().isOk
+        .expectBody()
+        .jsonPath("$.migrationId").isEqualTo("2020-01-01T00:00:00")
+        .jsonPath("$.status").isEqualTo("COMPLETED")
+    }
+  }
+
+  @Nested
+  @DisplayName("GET /migrate/history/active/{migrationType}")
+  inner class GetActiveMigration {
+    @BeforeEach
+    internal fun createHistoryRecords() {
+      runBlocking {
+        migrationHistoryRepository.deleteAll()
+        migrationHistoryRepository.save(
+          MigrationHistory(
+            migrationId = "2020-01-01T00:00:00",
+            whenStarted = LocalDateTime.parse("2020-01-01T00:00:00"),
+            whenEnded = LocalDateTime.parse("2020-01-01T01:00:00"),
+            status = MigrationStatus.STARTED,
+            estimatedRecordCount = 123_567,
+            filter = "",
+            recordsMigrated = 123_560,
+            recordsFailed = 7,
+            migrationType = VISIT_BALANCE,
+          ),
+        )
+        migrationHistoryRepository.save(
+          MigrationHistory(
+            migrationId = "2019-01-01T00:00:00",
+            whenStarted = LocalDateTime.parse("2019-01-01T00:00:00"),
+            whenEnded = LocalDateTime.parse("2019-01-01T01:00:00"),
+            status = MigrationStatus.COMPLETED,
+            estimatedRecordCount = 123_567,
+            filter = "",
+            recordsMigrated = 123_567,
+            recordsFailed = 0,
+            migrationType = VISIT_BALANCE,
+          ),
+        )
+      }
+    }
+
+    @AfterEach
+    internal fun deleteHistoryRecords() {
+      runBlocking {
+        migrationHistoryRepository.deleteAll()
+      }
+    }
+
+    @Nested
+    inner class Security {
+      @Test
+      fun `access forbidden when no role`() {
+        webTestClient.get().uri("/migrate/history/active/{migrationType}", VISIT_BALANCE)
+          .headers(setAuthorisation(roles = listOf()))
+          .exchange()
+          .expectStatus().isForbidden
+      }
+
+      @Test
+      fun `access forbidden with wrong role`() {
+        webTestClient.get().uri("/migrate/history/active/{migrationType}", VISIT_BALANCE)
+          .headers(setAuthorisation(roles = listOf("ROLE_BANANAS")))
+          .exchange()
+          .expectStatus().isForbidden
+      }
+
+      @Test
+      fun `access unauthorised with no auth token`() {
+        webTestClient.get().uri("/migrate/history/active/{migrationType}", VISIT_BALANCE)
+          .header("Content-Type", "application/json")
+          .exchange()
+          .expectStatus().isUnauthorized
+      }
+    }
+
+    @Test
+    internal fun `will return dto with null contents if no migrations are found`() {
+      deleteHistoryRecords()
+      webTestClient.get().uri("/migrate/history/active/{migrationType}", VISIT_BALANCE)
+        .headers(setAuthorisation(roles = listOf("PRISONER_FROM_NOMIS__MIGRATION__RW")))
+        .header("Content-Type", "application/json")
+        .exchange()
+        .expectStatus().isOk
+        .expectBody()
+        .jsonPath("$.migrationId").doesNotExist()
+        .jsonPath("$.whenStarted").doesNotExist()
+        .jsonPath("$.recordsMigrated").doesNotExist()
+        .jsonPath("$.estimatedRecordCount").doesNotExist()
+        .jsonPath("$.status").doesNotExist()
+        .jsonPath("$.migrationType").doesNotExist()
+    }
+
+    @Test
+    internal fun `can read active migration data`() {
+      mappingApiMock.stubGetMigrationDetails(migrationId = "2020-01-01T00%3A00%3A00", count = 123456)
+      webTestClient.get().uri("/migrate/history/active/{migrationType}", VISIT_BALANCE)
+        .headers(setAuthorisation(roles = listOf("PRISONER_FROM_NOMIS__MIGRATION__RW")))
+        .header("Content-Type", "application/json")
+        .exchange()
+        .expectStatus().isOk
+        .expectBody()
+        .jsonPath("$.migrationId").isEqualTo("2020-01-01T00:00:00")
+        .jsonPath("$.whenStarted").isEqualTo("2020-01-01T00:00:00")
+        .jsonPath("$.recordsMigrated").isEqualTo(123456)
+        .jsonPath("$.toBeProcessedCount").isEqualTo(0)
+        .jsonPath("$.beingProcessedCount").isEqualTo(0)
+        .jsonPath("$.recordsFailed").isEqualTo(0)
+        .jsonPath("$.estimatedRecordCount").isEqualTo(123567)
+        .jsonPath("$.status").isEqualTo("STARTED")
+        .jsonPath("$.migrationType").isEqualTo("VISIT_BALANCE")
+    }
+  }
+}
