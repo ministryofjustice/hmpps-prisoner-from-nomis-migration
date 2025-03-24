ef22fd17903cd912e0623e977ba358b1b96f7fb6
petergphillips <5099053+petergphillips@users.noreply.github.com>
2025-03-24 12:09:25 +0000

    SDIT-2682: 🔥 Remove csip migration (2) (#1236)

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/csip/CSIPNomisApiMockServer.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/csip/CSIPNomisApiMockServer.kt
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.github.tomakehurst.wiremock.client.CountMatchingStrategy
 import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder
 import com.github.tomakehurst.wiremock.client.WireMock.aResponse
-import com.github.tomakehurst.wiremock.client.WireMock.equalTo
 import com.github.tomakehurst.wiremock.client.WireMock.get
 import com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo
 import com.github.tomakehurst.wiremock.client.WireMock.urlPathMatching
@@ -27,16 +26,12 @@ import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomisprisoner.mod
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomisprisoner.model.Review
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomisprisoner.model.SaferCustodyScreening
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.wiremock.NomisApiExtension.Companion.nomisApi
-import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.wiremock.pageContent
-import java.lang.Long.min
 import java.time.LocalDate
 import java.time.LocalDateTime
 
 @Component
 class CSIPNomisApiMockServer(private val objectMapper: ObjectMapper) {
   companion object {
-    const val CSIP_ID_URL = "/csip/ids"
-
     fun nomisCSIPReport(nomisCSIPId: Long = 1234) = CSIPResponse(
       id = nomisCSIPId,
       offender = Offender("A1234BC", firstName = "Fred", lastName = "Smith"),
@@ -232,47 +227,6 @@ class CSIPNomisApiMockServer(private val objectMapper: ObjectMapper) {
     )
   }
 
-  fun stubGetPagedCSIPIds(totalElements: Long = 20, pageSize: Long = 20) {
-    // for each page create a response for each csip id starting from 1 up to `totalElements`
-
-    val pages = (totalElements / pageSize) + 1
-    (0..pages).forEach { page ->
-      val startId = (page * pageSize) + 1
-      val endId = min((page * pageSize) + pageSize, totalElements)
-      nomisApi.stubFor(
-        get(
-          urlPathEqualTo("/csip/ids"),
-        )
-          .withQueryParam("page", equalTo(page.toString()))
-          .willReturn(
-            aResponse().withHeader("Content-Type", "application/json").withStatus(HttpStatus.OK.value())
-              .withBody(
-                csipIdsPagedResponse(
-                  totalElements = totalElements,
-                  ids = (startId..endId).map { it },
-                  pageNumber = page,
-                  pageSize = pageSize,
-                ),
-              ),
-          ),
-      )
-    }
-  }
-
-  fun stubMultipleGetCSIP(intProgression: IntProgression) {
-    (intProgression).forEach {
-      nomisApi.stubFor(
-        get(
-          urlPathEqualTo("/csip/$it"),
-        )
-          .willReturn(
-            aResponse().withHeader("Content-Type", "application/json").withStatus(HttpStatus.OK.value())
-              .withBody(nomisCSIPReport(it.toLong())),
-          ),
-      )
-    }
-  }
-
   fun stubGetCSIP(nomisCSIPId: Long = 1234) {
     nomisApi.stubFor(
       get(urlPathEqualTo("/csip/$nomisCSIPId"))
@@ -356,21 +310,9 @@ class CSIPNomisApiMockServer(private val objectMapper: ObjectMapper) {
     )
   }
 
-  fun stubGetInitialCount(urlPath: String, totalElements: Long, pagedResponse: (totalElements: Long) -> String) = nomisApi.stubGetInitialCount(urlPath, totalElements, pagedResponse)
-  fun verifyGetIdsCount(url: String, fromDate: String, toDate: String, prisonId: String? = null) = nomisApi.verifyGetIdsCount(url, fromDate, toDate, prisonId)
   fun verify(countMatchingStrategy: CountMatchingStrategy, requestPatternBuilder: RequestPatternBuilder) = nomisApi.verify(countMatchingStrategy, requestPatternBuilder)
   fun verify(pattern: RequestPatternBuilder) = nomisApi.verify(pattern)
   fun verify(count: Int, pattern: RequestPatternBuilder) = nomisApi.verify(count, pattern)
 
   fun ResponseDefinitionBuilder.withBody(body: Any): ResponseDefinitionBuilder = this.withBody(objectMapper.writeValueAsString(body))
-}
-
-fun csipIdsPagedResponse(
-  totalElements: Long = 10,
-  ids: List<Long> = (0L..10L).toList(),
-  pageSize: Long = 10,
-  pageNumber: Long = 0,
-): String {
-  val content = ids.map { """{ "csipId": $it }""" }.joinToString { it }
-  return pageContent(content, pageSize, pageNumber, totalElements, ids.size)
 }
