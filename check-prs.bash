c1a8488bad72bd57a842f3fdbca714b133a0b780
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2025-04-04 14:29:13 +0100

    SDIT-2181: 🔨 Sort out check PRs formatting

--- check-prs.bash
+++ check-prs.bash
@@ -8,27 +8,18 @@ else
   OUTPUT_COMPARISON=false
 fi
 
-FAILING_URLS=$(gh pr ls --search 'head:api-docs-' --json 'statusCheckRollup,url' --jq '. | map(select(.statusCheckRollup[].state == "FAILURE")) | .[].url' | sort -u)
-if [[ $FAILING_URLS != "" ]]; then
-  {
-    echo "failing_prs<<EOF"
-    echo "$FAILING_URLS"
-    echo "EOF"
-  } >>"$GITHUB_OUTPUT"
+FAILURE_COUNT=$(gh pr ls --search 'head:api-docs-' --json 'statusCheckRollup,url' --jq '. | map(select(.statusCheckRollup[].state == "FAILURE")) | .[].url' | sort -u | wc -l | awk '{print $1}')
+if [[ $FAILURE_COUNT != "0" ]]; then
+  echo "failure_count=$FAILURE_COUNT" >>"$GITHUB_OUTPUT"
 else
-  echo "failing_prs=" >>"$GITHUB_OUTPUT"
+  echo "failure_count=" >>"$GITHUB_OUTPUT"
 fi
 
-URLS=$(gh pr ls --search 'head:api-docs-' --json 'url' --jq '.[].url' | sort -u)
-if [[ $URLS != "" ]]; then
-  {
-    echo "all_prs<<EOF"
-    echo "$URLS"
-    echo "EOF"
-  } >>"$GITHUB_OUTPUT"
-else
-  echo "all_prs=" >>"$GITHUB_OUTPUT"
-fi
+COMBINED_PR=$(gh pr ls --search 'head:combined-prs-branch' --json 'url' --jq '.[].url' | sort -u)
+echo "combined_pr=$COMBINED_PR" >>"$GITHUB_OUTPUT"
+
+PR_COUNT=$(gh pr ls --search 'head:api-docs-' --json 'url' --jq '.[].url' | sort -u | wc -l | awk '{print $1}')
+echo "pr_count=$PR_COUNT" >>"$GITHUB_OUTPUT"
 
 if [[ $OUTPUT_COMPARISON = true ]]; then
   cat "$GITHUB_OUTPUT"
--- enable-auto-merge-pull-request.bash
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-PR_NUMBER=${1?No PR specified}
-
-gh pr merge --auto --squash "$PR_NUMBER"
