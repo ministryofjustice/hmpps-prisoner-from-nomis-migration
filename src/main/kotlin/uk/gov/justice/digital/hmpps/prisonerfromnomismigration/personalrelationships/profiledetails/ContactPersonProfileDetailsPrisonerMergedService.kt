0e10380d52ee98a3cd8eaec8791164807924a9a3
Mike Halma <mike.halma@digital.justice.gov.uk>
2025-04-15 14:58:18 +0100

    SDIT-2693 Sync back to NOMIS after merge

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/personalrelationships/profiledetails/ContactPersonProfileDetailsPrisonerMergedService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/personalrelationships/profiledetails/ContactPersonProfileDetailsPrisonerMergedService.kt
@@ -8,6 +8,7 @@ import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.data.PrisonerMerg
 @Service
 class ContactPersonProfileDetailsPrisonerMergedService(
   private val dpsApiService: ContactPersonProfileDetailsDpsApiService,
+  private val nomisSyncApiService: ContactPersonProfileDetailsNomisSyncApiService,
   private val telemetryClient: TelemetryClient,
 ) {
 
@@ -23,11 +24,25 @@ class ContactPersonProfileDetailsPrisonerMergedService(
 
     runCatching {
       dpsApiService.mergeProfileDetails(keepingPrisonerNumber, removedPrisonerNumber)
+      syncOffenderToNomis(keepingPrisonerNumber, telemetry)
       telemetryClient.trackEvent("contact-person-profile-details-prisoner-merged", telemetry, null)
     }.onFailure { e ->
       telemetry["error"] = e.message.toString()
       telemetryClient.trackEvent("contact-person-profile-details-prisoner-merged-error", telemetry.toMap())
       throw e
     }
+  }
+
+  suspend fun syncOffenderToNomis(offenderNo: String, telemetry: MutableMap<String, String>) {
+    ContactPersonProfileType.all().forEach { profileType ->
+      nomisSyncApiService.syncProfileDetails(offenderNo, profileType)
+        .also { telemetry.addToTelemetry("syncToNomis", "$offenderNo-$profileType") }
+    }
+  }
+
+  private fun MutableMap<String, String>.addToTelemetry(telemetryKey: String, addProfileType: String) {
+    val profileTypes = this[telemetryKey]
+    val newProfileTypes = profileTypes?.split(",")?.plus(addProfileType) ?: listOf(addProfileType)
+    this[telemetryKey] = newProfileTypes.joinToString(",")
   }
 }
