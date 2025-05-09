35c00348f42c56540d4d7bbd8145c8ffd618360c
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2025-05-09 09:07:03 +0100

    SDIT-2632 Add client for activities endpoint move activity start dates (#1372)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/activities/ActivitiesApiService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/activities/ActivitiesApiService.kt
@@ -4,10 +4,12 @@ import kotlinx.coroutines.reactor.awaitSingle
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.bodyToMono
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.activities.model.ActivityMigrateRequest
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.activities.model.ActivityMigrateResponse
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.activities.model.AllocationMigrateRequest
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.activities.model.AllocationMigrateResponse
+import java.time.LocalDate
 
 @Service
 class ActivitiesApiService(@Qualifier("activitiesApiWebClient") private val webClient: WebClient) {
@@ -16,13 +18,23 @@ class ActivitiesApiService(@Qualifier("activitiesApiWebClient") private val webC
     .uri("/migrate/activity")
     .bodyValue(migrateRequest)
     .retrieve()
-    .bodyToMono(ActivityMigrateResponse::class.java)
-    .awaitSingle()!!
+    .bodyToMono<ActivityMigrateResponse>()
+    .awaitSingle()
 
   suspend fun migrateAllocation(migrateRequest: AllocationMigrateRequest): AllocationMigrateResponse = webClient.post()
     .uri("/migrate/allocation")
     .bodyValue(migrateRequest)
     .retrieve()
-    .bodyToMono(AllocationMigrateResponse::class.java)
-    .awaitSingle()!!
+    .bodyToMono<AllocationMigrateResponse>()
+    .awaitSingle()
+
+  suspend fun moveActivityStartDates(prisonCode: String, newStartDate: LocalDate): List<String> = webClient.post()
+    .uri {
+      it.path("/migrate/{prisonCode}/move-activity-start-dates")
+        .queryParam("activityStartDate", newStartDate)
+        .build(prisonCode)
+    }
+    .retrieve()
+    .bodyToMono<List<String>>()
+    .awaitSingle()
 }
