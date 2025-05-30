cbbbe28cb69f0cb6492cd785a4c20debd337ec2b
Karen Miller <75248289+karenmillermoj@users.noreply.github.com>
2025-05-30 10:34:43 +0100

    SDIT-2737 Add git hub actions (#1412)
    * SDIT-2737: ✨ Initial stab at separating tests into different tasks
    
    * SDIT-2737 Separate tests
    
    * SDIT-2737 swap quay references for ghcr
    
    * SDIT-2737 swap quay references for ghcr
    
    * SDIT-2737 Pick up all tests
    
    * SDIT-2737 Fix test matrix naming
    
    * SDIT-2737 Fix test matrix naming - 2
    
    ---------
    
    Co-authored-by: Peter Phillips <5099053+petergphillips@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -63,10 +63,11 @@ kotlin {
   }
 }
 
-data class ModelConfiguration(val name: String, val packageName: String, val url: String, val models: String = "") {
+data class ModelConfiguration(val name: String, val packageName: String, val testPackageName: String? = null, val url: String, val models: String = "") {
   fun toBuildModelTaskName(): String = "build${nameToCamel()}ApiModel"
   fun toWriteJsonTaskName(): String = "write${nameToCamel()}Json"
   fun toReadProductionVersionTaskName(): String = "read${nameToCamel()}ProductionVersion"
+  fun toTestTaskName(): String = "test${nameToCamel()}"
   private val snakeRegex = "-[a-zA-Z]".toRegex()
   private fun nameToCamel(): String = snakeRegex.replace(name) {
     it.value.replace("-", "").uppercase()
@@ -81,47 +82,55 @@ val models = listOf(
   ModelConfiguration(
     name = "activities",
     packageName = "activities",
+    testPackageName = "activities",
     url = "https://activities-api-dev.prison.service.justice.gov.uk/v3/api-docs",
     models = "ActivityMigrateRequest,ActivityMigrateResponse,AllocationMigrateRequest,AllocationMigrateResponse,AppointmentInstance,AppointmentMigrateRequest,NomisPayRate,NomisScheduleRule,Slot",
   ),
   ModelConfiguration(
     name = "alerts",
     packageName = "alerts",
+    testPackageName = "alerts",
     url = "https://alerts-api-dev.hmpps.service.justice.gov.uk/v3/api-docs",
     models = "Alert,AlertCodeSummary,CreateAlert,ResyncAlert,ResyncedAlert,UpdateAlert",
   ),
   ModelConfiguration(
     name = "casenotes",
     packageName = "casenotes",
+    testPackageName = "casenotes",
     url = "https://dev.offender-case-notes.service.justice.gov.uk/v3/api-docs",
     models = "Author,ErrorResponse,MoveCaseNotesRequest,SyncCaseNoteAmendmentRequest,SyncCaseNoteRequest,SyncResult",
   ),
   ModelConfiguration(
     name = "core-person",
     packageName = "coreperson",
+    testPackageName = "coreperson",
     url = "https://hmpps-person-record-dev.hmpps.service.justice.gov.uk/v3/api-docs",
     models = "Address,AddressId,CreateResponse,Email,EmailId,Identifier,Names,PhoneId,PhoneNumber,Prisoner,Religion",
   ),
   ModelConfiguration(
     name = "court-sentencing",
     packageName = "courtsentencing",
+    testPackageName = "courtsentencing",
     url = "https://remand-and-sentencing-api-dev.hmpps.service.justice.gov.uk/v3/api-docs",
   ),
   ModelConfiguration(
     name = "csip",
     packageName = "csip",
+    testPackageName = "csip",
     url = "https://csip-api-dev.hmpps.service.justice.gov.uk/v3/api-docs",
     models = "DefaultLegacyActioned,MoveCsipRequest,PersonSummaryRequest,ResponseMapping,SyncAttendeeRequest,SyncContributoryFactorRequest,SyncCsipRequest,SyncDecisionAndActionsRequest,SyncInterviewRequest,SyncInvestigationRequest,SyncNeedRequest,SyncPlanRequest,SyncReferralRequest,SyncResponse,SyncReviewRequest,SyncScreeningOutcomeRequest",
   ),
   ModelConfiguration(
     name = "incidents",
     packageName = "incidents",
+    testPackageName = "incidents",
     url = "https://incident-reporting-api-dev.hmpps.service.justice.gov.uk/v3/api-docs",
     models = "CorrectionRequest,DescriptionAddendum,Event,HistoricalQuestion,HistoricalResponse,History,NomisCode,NomisHistory,NomisHistoryQuestion,NomisHistoryResponse,NomisOffender,NomisOffenderParty,NomisQuestion,NomisReport,NomisRequirement,NomisResponse,NomisStaff,NomisStaffParty,NomisStatus,NomisSyncReportId,NomisSyncRequest,PairStringListDescriptionAddendum,PrisonerInvolvement,Question,ReportBasic,ReportWithDetails,Response,SimplePageReportBasic,StaffInvolvement,StatusHistory",
   ),
   ModelConfiguration(
     name = "locations",
     packageName = "locations",
+    testPackageName = "locations",
     url = "https://locations-inside-prison-api-dev.hmpps.service.justice.gov.uk/v3/api-docs",
     models = "Capacity,Certification,ChangeHistory,ErrorResponse,LegacyLocation,NomisSyncLocationRequest,NonResidentialUsageDto",
   ),
@@ -138,22 +147,26 @@ val models = listOf(
   ModelConfiguration(
     name = "organisations",
     packageName = "organisations",
+    testPackageName = "organisations",
     url = "https://organisations-api-dev.hmpps.service.justice.gov.uk/v3/api-docs",
   ),
   ModelConfiguration(
     name = "personal-relationships",
     packageName = "personalrelationships",
+    testPackageName = "personalrelationships",
     url = "https://personal-relationships-api-dev.hmpps.service.justice.gov.uk/v3/api-docs",
   ),
   ModelConfiguration(
     name = "sentencing-adjustments",
     packageName = "sentencing.adjustments",
+    testPackageName = "sentencing",
     url = "https://adjustments-api-dev.hmpps.service.justice.gov.uk/v3/api-docs",
     models = "LegacyAdjustment,LegacyAdjustmentCreatedResponse",
   ),
   ModelConfiguration(
     name = "visit-balance",
     packageName = "visit.balance",
+    testPackageName = "visitbalances",
     url = "https://hmpps-visit-allocation-api-dev.prison.service.justice.gov.uk/v3/api-docs",
     models = "VisitAllocationPrisonerMigrationDto,VisitAllocationPrisonerSyncDto",
   ),
@@ -173,6 +186,7 @@ tasks {
     mustRunAfter(models.map { it.toBuildModelTaskName() })
   }
 }
+val separateTestPackages = mutableListOf<String>()
 models.forEach {
   tasks.register(it.toBuildModelTaskName(), GenerateTask::class) {
     group = "Generate model from API JSON definition"
@@ -209,6 +223,27 @@ models.forEach {
       val json = URI.create(productionUrl).toURL().readText()
       val version = ObjectMapper().readTree(json).at("/build/version").asText()
       println(version)
+    }
+  }
+  if (it.testPackageName != null) {
+    separateTestPackages.add(it.testPackageName)
+    val task = tasks.register(it.toTestTaskName(), Test::class) {
+      group = "Run tests"
+      description = "Run tests for ${it.name}"
+      shouldRunAfter("test")
+      useJUnitPlatform()
+      filter {
+        includeTestsMatching("uk.gov.justice.digital.hmpps.prisonerfromnomismigration.${it.testPackageName}.*")
+      }
+    }
+    tasks.check { dependsOn(task) }
+  }
+}
+
+tasks.test {
+  filter {
+    separateTestPackages.forEach {
+      excludeTestsMatching("uk.gov.justice.digital.hmpps.prisonerfromnomismigration.$it.*")
     }
   }
 }
