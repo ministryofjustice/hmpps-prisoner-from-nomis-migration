14cf2062c155d9ed00f4229e68aec43197dffb3d
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2025-07-22 18:05:54 +0100

    SDIT-2948 - remove finance health check, they're only in dev so far (#1573)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/finance/FinanceConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/finance/FinanceConfiguration.kt
@@ -1,15 +1,12 @@
 package uk.gov.justice.digital.hmpps.prisonerfromnomismigration.finance
 
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientManager
-import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 import uk.gov.justice.hmpps.kotlin.auth.reactiveAuthorisedWebClient
 import uk.gov.justice.hmpps.kotlin.auth.reactiveHealthWebClient
-import uk.gov.justice.hmpps.kotlin.health.ReactiveHealthPingCheck
 import java.time.Duration
 
 @Configuration
@@ -27,7 +24,7 @@ class FinanceConfiguration(
     authorizedClientManager: ReactiveOAuth2AuthorizedClientManager,
     builder: WebClient.Builder,
   ): WebClient = builder.reactiveAuthorisedWebClient(authorizedClientManager, registrationId = "finance-api", url = apiBaseUri, timeout)
-
-  @Component("financeApi")
-  class FinanceApiHealth(@Qualifier("financeApiHealthWebClient") webClient: WebClient) : ReactiveHealthPingCheck(webClient)
+//
+//  @Component("financeApi")
+//  class FinanceApiHealth(@Qualifier("financeApiHealthWebClient") webClient: WebClient) : ReactiveHealthPingCheck(webClient)
 }
