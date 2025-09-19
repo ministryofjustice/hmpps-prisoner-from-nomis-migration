bf93db1fd997066d7a4604c7709bdb7ab743a61d
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2025-09-19 11:52:40 +0100

    SDIT-3054: ✨ Use generated api to call nomis mapping

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/finance/TransactionMappingApiService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/finance/TransactionMappingApiService.kt
@@ -5,25 +5,15 @@ import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.helpers.awaitBodyOrNullWhenNotFound
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.integration.history.MigrationMapping
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomismappings.api.TransactionMappingResourceApi
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomismappings.model.TransactionMappingDto
 
 @Service
 class TransactionMappingApiService(@Qualifier("mappingApiWebClient") webClient: WebClient) : MigrationMapping<TransactionMappingDto>(domainUrl = "/mapping/transactions", webClient) {
-  suspend fun getMappingGivenNomisIdOrNull(transactionId: Long): TransactionMappingDto? = webClient.get()
-    .uri("/mapping/transactions/nomis-transaction-id/{transactionId}", transactionId)
+  private val api = TransactionMappingResourceApi(webClient)
+
+  suspend fun getMappingGivenNomisIdOrNull(transactionId: Long): TransactionMappingDto? = api
+    .prepare(api.getMappingByNomisId3RequestConfig(transactionId))
     .retrieve()
     .awaitBodyOrNullWhenNotFound()
-
-// TODO suspend fun updateMappingsByNomisId(oldOffenderNo: String, newOffenderNo: String) {
-//    webClient.put()
-//      .uri("/mapping/transactions/merge/from/{oldOffenderNo}/to/{newOffenderNo}", oldOffenderNo, newOffenderNo)
-//      .retrieve()
-//      .awaitBodilessEntity()
-//  }
-//
-//  suspend fun updateMappingsByBookingId(bookingId: Long, newOffenderNo: String): List<TransactionMappingDto> =
-//    webClient.put()
-//      .uri("/mapping/transactions/merge/booking-id/{bookingId}/to/{newOffenderNo}", bookingId, newOffenderNo)
-//      .retrieve()
-//      .awaitBody()
 }
--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/visitbalances/VisitBalanceMappingApiService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/visitbalances/VisitBalanceMappingApiService.kt
@@ -12,15 +12,18 @@ import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.helpers.awaitBody
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.integration.history.CreateMappingResult
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.integration.history.DuplicateErrorResponse
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.integration.history.MigrationMapping
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomismappings.api.VisitBalanceAdjustmentMappingResourceApi
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomismappings.api.VisitBalanceMappingResourceApi
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomismappings.model.VisitBalanceAdjustmentMappingDto
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomismappings.model.VisitBalanceMappingDto
 
 @Service
 class VisitBalanceMappingApiService(@Qualifier("mappingApiWebClient") webClient: WebClient) : MigrationMapping<VisitBalanceMappingDto>(domainUrl = "/mapping/visit-balance", webClient) {
+  private val api = VisitBalanceMappingResourceApi(webClient)
+  private val adjustmentApi = VisitBalanceAdjustmentMappingResourceApi(webClient)
 
-  suspend fun createVisitBalanceMapping(mappings: VisitBalanceMappingDto): CreateMappingResult<VisitBalanceMappingDto> = webClient.post()
-    .uri("/mapping/visit-balance")
-    .bodyValue(mappings)
+  suspend fun createVisitBalanceMapping(mappings: VisitBalanceMappingDto): CreateMappingResult<VisitBalanceMappingDto> = api
+    .prepare(api.createMapping3RequestConfig(mappings))
     .retrieve()
     .bodyToMono(Unit::class.java)
     .map { CreateMappingResult<VisitBalanceMappingDto>() }
@@ -29,25 +32,18 @@ class VisitBalanceMappingApiService(@Qualifier("mappingApiWebClient") webClient:
     }
     .awaitFirstOrDefault(CreateMappingResult())
 
-  suspend fun getByNomisVisitBalanceIdOrNull(nomisVisitBalanceId: Long): VisitBalanceMappingDto? = webClient.get()
-    .uri(
-      "/mapping/visit-balance/nomis-id/{nomisVisitBalanceId}",
-      nomisVisitBalanceId,
-    )
+  suspend fun getByNomisVisitBalanceIdOrNull(nomisVisitBalanceId: Long): VisitBalanceMappingDto? = api
+    .prepare(api.getMappingByNomisId1RequestConfig(nomisVisitBalanceId))
     .retrieve()
     .awaitBodyOrNullWhenNotFound()
 
-  suspend fun getByNomisVisitBalanceId(nomisVisitBalanceId: Long): VisitBalanceMappingDto = webClient.get()
-    .uri(
-      "/mapping/visit-balance/nomis-id/{nomisVisitBalanceId}",
-      nomisVisitBalanceId,
-    )
+  suspend fun getByNomisVisitBalanceId(nomisVisitBalanceId: Long): VisitBalanceMappingDto = api
+    .prepare(api.getMappingByNomisId1RequestConfig(nomisVisitBalanceId))
     .retrieve()
     .awaitBody()
 
-  suspend fun createVisitBalanceAdjustmentMapping(mapping: VisitBalanceAdjustmentMappingDto): CreateMappingResult<VisitBalanceAdjustmentMappingDto> = webClient.post()
-    .uri("/mapping/visit-balance-adjustment")
-    .bodyValue(mapping)
+  suspend fun createVisitBalanceAdjustmentMapping(mapping: VisitBalanceAdjustmentMappingDto): CreateMappingResult<VisitBalanceAdjustmentMappingDto> = adjustmentApi
+    .prepare(adjustmentApi.createMapping2RequestConfig(mapping))
     .retrieve()
     .bodyToMono(Unit::class.java)
     .map { CreateMappingResult<VisitBalanceAdjustmentMappingDto>() }
@@ -56,11 +52,8 @@ class VisitBalanceMappingApiService(@Qualifier("mappingApiWebClient") webClient:
     }
     .awaitFirstOrDefault(CreateMappingResult())
 
-  suspend fun getByNomisVisitBalanceAdjustmentIdOrNull(nomisVisitBalanceAdjustmentId: Long): VisitBalanceAdjustmentMappingDto? = webClient.get()
-    .uri(
-      "/mapping/visit-balance-adjustment/nomis-id/{nomisVisitBalanceAdjustmentId}",
-      nomisVisitBalanceAdjustmentId,
-    )
+  suspend fun getByNomisVisitBalanceAdjustmentIdOrNull(nomisVisitBalanceAdjustmentId: Long): VisitBalanceAdjustmentMappingDto? = adjustmentApi
+    .prepare(adjustmentApi.getMappingByNomisId2RequestConfig(nomisVisitBalanceAdjustmentId))
     .retrieve()
     .awaitBodyOrNullWhenNotFound()
 }
