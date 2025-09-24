725d86ad9677c5fe19bdd9efbea0c442d647da29
Mike Halma <mike.halma@digital.justice.gov.uk>
2025-09-24 01:14:22 +0100

    SDIT-2845 Remove external movements health check to stop it failing in prod

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/movements/ExternalMovementsConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/movements/ExternalMovementsConfiguration.kt
@@ -1,15 +1,12 @@
 package uk.gov.justice.digital.hmpps.prisonerfromnomismigration.movements
 
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientManager
-import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 import uk.gov.justice.hmpps.kotlin.auth.reactiveAuthorisedWebClient
 import uk.gov.justice.hmpps.kotlin.auth.reactiveHealthWebClient
-import uk.gov.justice.hmpps.kotlin.health.ReactiveHealthPingCheck
 import java.time.Duration
 
 @Configuration
@@ -28,6 +25,7 @@ class ExternalMovementsConfiguration(
   @Bean
   fun extMovementsApiHealthWebClient(builder: WebClient.Builder): WebClient = builder.reactiveHealthWebClient(apiBaseUri, healthTimeout)
 
-  @Component("extMovementsApi")
-  class MovementsApiHealth(@Qualifier("extMovementsApiHealthWebClient") webClient: WebClient) : ReactiveHealthPingCheck(webClient)
+//  TODO reinstate this once the external-movements-api is deployed to production
+//  @Component("extMovementsApi")
+//  class MovementsApiHealth(@Qualifier("extMovementsApiHealthWebClient") webClient: WebClient) : ReactiveHealthPingCheck(webClient)
 }
