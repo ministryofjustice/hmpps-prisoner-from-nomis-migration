9aa514ed0d52f88318c37ed32279790a422b335f
petergphillips <5099053+petergphillips@users.noreply.github.com>
2025-09-26 16:31:33 +0100

    SDIT-3066: 🐛 Try out fix for pageable swagger generation (#1793)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/finance/PrisonerBalanceNomisApiService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/finance/PrisonerBalanceNomisApiService.kt
@@ -4,29 +4,22 @@ import kotlinx.coroutines.reactive.awaitSingle
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
-import org.springframework.web.reactive.function.client.awaitBody
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomisprisoner.api.PrisonerBalanceResourceApi
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomisprisoner.model.PrisonerBalanceDto
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.service.RestResponsePagedModel
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.service.typeReference
 
 @Service
-class PrisonerBalanceNomisApiService(@Qualifier("nomisApiWebClient") private val webClient: WebClient) {
+class PrisonerBalanceNomisApiService(@Qualifier("nomisApiWebClient") webClient: WebClient) {
   private val api = PrisonerBalanceResourceApi(webClient)
 
-  suspend fun getRootOffenderIdsToMigrate(prisonId: String?, pageNumber: Long, pageSize: Long): RestResponsePagedModel<Long> = webClient.get().uri {
-    it.path("/finance/prisoners/ids")
-      .queryParam("page", pageNumber)
-      .queryParam("size", pageSize)
-      .queryParam("prisonId", prisonId)
-      .build()
-  }
+  suspend fun getRootOffenderIdsToMigrate(prisonId: String?, pageNumber: Long, pageSize: Long): RestResponsePagedModel<Long> = api
+    .prepare(api.getPrisonerBalanceIdentifiersRequestConfig(page = pageNumber.toInt(), size = pageSize.toInt(), sort = null, prisonId = prisonId))
     .retrieve()
     .bodyToMono(typeReference<RestResponsePagedModel<Long>>())
     .awaitSingle()
 
   suspend fun getPrisonerBalance(rootOffenderId: Long): PrisonerBalanceDto = api
-    .prepare(api.getPrisonerAccountDetailsRequestConfig(rootOffenderId))
-    .retrieve()
-    .awaitBody()
+    .getPrisonerAccountDetails(rootOffenderId)
+    .awaitSingle()
 }
