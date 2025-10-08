9b989803bbbf68af5b2228824ecc663035850fee
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2025-10-08 11:46:24 +0100

    SDIT-3046 Use the latest version of the SQS lib (#1834)

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ configurations {
 dependencies {
   implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.7.0")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.5.0-beta-2")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.5.0")
   implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
 
   implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.8.13")
