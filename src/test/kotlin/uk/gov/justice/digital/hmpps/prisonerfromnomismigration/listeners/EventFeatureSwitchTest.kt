401ef2ff4c6227a5639ef006c1afe15d14955957
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2025-10-15 10:46:44 +0100

    SDIT-3102: 🎨 Make domain mandatory

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/listeners/EventFeatureSwitchTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/listeners/EventFeatureSwitchTest.kt
@@ -21,35 +21,32 @@ internal class EventFeatureSwitchTest : SqsIntegrationTestBase() {
 
   @Test
   fun `should return true when feature is enabled`() {
-    assertThat(featureSwitch.isEnabled("IEP_UPSERTED")).isTrue
+    assertThat(featureSwitch.isEnabled("IEP_UPSERTED", domain = "billybob")).isTrue
   }
 
   @Test
   fun `should return false when feature is disabled`() {
-    assertThat(featureSwitch.isEnabled("OTHER_EVENT")).isFalse
+    assertThat(featureSwitch.isEnabled("OTHER_EVENT", domain = "billybob")).isFalse
   }
 
   @Test
   fun `should return true when feature switch is not present`() {
-    assertThat(featureSwitch.isEnabled("NO_SWITCH_EVENT")).isTrue
+    assertThat(featureSwitch.isEnabled("NO_SWITCH_EVENT", domain = "billybob")).isTrue
   }
 
-  @Nested
-  inner class DomainSwitch {
-    @Test
-    fun `should return true when feature switch is not present `() {
-      assertThat(featureSwitch.isEnabled("GENERIC_EVENT")).isTrue
-    }
+  @Test
+  fun `should return true when feature switch is not present for that domain`() {
+    assertThat(featureSwitch.isEnabled("GENERIC_EVENT", domain = "billybob")).isTrue
+  }
 
-    @Test
-    fun `should return true when feature switch is not present for domain `() {
-      assertThat(featureSwitch.isEnabled("GENERIC_EVENT", domain = "alert")).isTrue
-    }
+  @Test
+  fun `should return true when feature switch is not present for domain`() {
+    assertThat(featureSwitch.isEnabled("GENERIC_EVENT", domain = "alert")).isTrue
+  }
 
-    @Test
-    fun `should return false when feature switch is present for domain `() {
-      assertThat(featureSwitch.isEnabled("GENERIC_EVENT", domain = "casenote")).isFalse()
-    }
+  @Test
+  fun `should return false when feature switch is present for domain`() {
+    assertThat(featureSwitch.isEnabled("GENERIC_EVENT", domain = "casenote")).isFalse()
   }
 
   @Nested
