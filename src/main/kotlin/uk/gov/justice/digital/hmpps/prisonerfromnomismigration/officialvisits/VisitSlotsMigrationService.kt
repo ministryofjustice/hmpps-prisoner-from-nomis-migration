4e5a7e9bb32cfc189ecbc6cd90bb6f987ef0213f
Andy Marke <andy.marke@digital.justice.gov.uk>
2025-11-05 12:18:10 +0000

    SDIT-3143 Basic migration (#1937)
    Only works if everything is migrated so pretty useless

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/officialvisits/VisitSlotsMigrationService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/officialvisits/VisitSlotsMigrationService.kt
@@ -1,36 +1,72 @@
 package uk.gov.justice.digital.hmpps.prisonerfromnomismigration.officialvisits
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.data.MigrationContext
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomismappings.model.VisitTimeSlotMigrationMappingDto
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomisprisoner.model.VisitTimeSlotIdResponse
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.service.MigrationService
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.service.MigrationType
+import java.time.DayOfWeek
 
 @Service
 class VisitSlotsMigrationService(
-  mappingService: VisitSlotsMappingService,
-) : MigrationService<Any, Any, VisitTimeSlotMigrationMappingDto>(
-  mappingService,
-  MigrationType.VISIT_SLOTS,
-  pageSize = 1,
-  completeCheckDelaySeconds = 1,
-  completeCheckCount = 1,
-  completeCheckRetrySeconds = 1,
-  completeCheckScheduledRetrySeconds = 1,
+  private val visitSlotsMappingService: VisitSlotsMappingService,
+  private val nomisApiService: VisitSlotsNomisApiService,
+  @Value("\${visitslots.page.size:1000}") pageSize: Long,
+  @Value("\${visitslots.complete-check.delay-seconds}") completeCheckDelaySeconds: Int,
+  @Value("\${visitslots.complete-check.retry-seconds:1}") completeCheckRetrySeconds: Int,
+  @Value("\${visitslots.complete-check.count}") completeCheckCount: Int,
+  @Value("\${complete-check.scheduled-retry-seconds}") completeCheckScheduledRetrySeconds: Int,
+) : MigrationService<Any, VisitTimeSlotIdResponse, VisitTimeSlotMigrationMappingDto>(
+  mappingService = visitSlotsMappingService,
+  migrationType = MigrationType.VISIT_SLOTS,
+  pageSize = pageSize,
+  completeCheckDelaySeconds = completeCheckDelaySeconds,
+  completeCheckCount = completeCheckRetrySeconds,
+  completeCheckRetrySeconds = completeCheckCount,
+  completeCheckScheduledRetrySeconds = completeCheckScheduledRetrySeconds,
 ) {
+  private companion object {
+    val log: Logger = LoggerFactory.getLogger(this::class.java)
+  }
+
   override suspend fun getPageOfIds(
     migrationFilter: Any,
     pageSize: Long,
     pageNumber: Long,
-  ): List<Any> {
-    TODO("Not yet implemented")
-  }
+  ): List<VisitTimeSlotIdResponse> = nomisApiService.getVisitTimeSlotIds(
+    pageNumber = pageNumber,
+    pageSize = pageSize,
+  ).content!!
 
-  override suspend fun getTotalNumberOfIds(migrationFilter: Any): Long {
-    TODO("Not yet implemented")
-  }
+  override suspend fun getTotalNumberOfIds(migrationFilter: Any): Long = nomisApiService.getVisitTimeSlotIds(
+    pageNumber = 0,
+    pageSize = 1,
+  ).page!!.totalElements!!
 
-  override suspend fun migrateNomisEntity(context: MigrationContext<Any>) {
-    TODO("Not yet implemented")
+  override suspend fun migrateNomisEntity(context: MigrationContext<VisitTimeSlotIdResponse>) {
+    val nomisId = context.body
+    val alreadyMigratedMapping = visitSlotsMappingService.getByNomisIdsOrNull(
+      nomisPrisonId = nomisId.prisonId,
+      nomisDayOfWeek = nomisId.dayOfWeek.asMappingDayOfWeek(),
+      nomisSlotSequence = nomisId.timeSlotSequence,
+    )
+
+    alreadyMigratedMapping?.run {
+      log.info("Will not migrate the nomis visit time slot=$nomisPrisonId;$nomisDayOfWeek;$nomisSlotSequence since it was already mapped to DPS visits slot time $dpsId during migration $label")
+    }
   }
+}
+
+private fun VisitTimeSlotIdResponse.DayOfWeek.asMappingDayOfWeek(): DayOfWeek = when (this) {
+  VisitTimeSlotIdResponse.DayOfWeek.MONDAY -> DayOfWeek.MONDAY
+  VisitTimeSlotIdResponse.DayOfWeek.TUESDAY -> DayOfWeek.TUESDAY
+  VisitTimeSlotIdResponse.DayOfWeek.WEDNESDAY -> DayOfWeek.WEDNESDAY
+  VisitTimeSlotIdResponse.DayOfWeek.THURSDAY -> DayOfWeek.THURSDAY
+  VisitTimeSlotIdResponse.DayOfWeek.FRIDAY -> DayOfWeek.FRIDAY
+  VisitTimeSlotIdResponse.DayOfWeek.SATURDAY -> DayOfWeek.SATURDAY
+  VisitTimeSlotIdResponse.DayOfWeek.SUNDAY -> DayOfWeek.SUNDAY
 }
--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/officialvisits/VisitSlotsNomisApiService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/officialvisits/VisitSlotsNomisApiService.kt
@@ -2,16 +2,11 @@ package uk.gov.justice.digital.hmpps.prisonerfromnomismigration.officialvisits
 
 import kotlinx.coroutines.reactor.awaitSingle
 import org.springframework.beans.factory.annotation.Qualifier
-import org.springframework.data.domain.PageImpl
-import org.springframework.data.domain.PageRequest
-import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomisprisoner.api.VisitsConfigurationResourceApi
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomisprisoner.api.VisitsConfigurationResourceApi.DayOfWeekGetVisitTimeSlot
-import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomisprisoner.model.PageMetadata
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomisprisoner.model.PagedModelVisitTimeSlotIdResponse
-import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomisprisoner.model.VisitTimeSlotIdResponse
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomisprisoner.model.VisitTimeSlotResponse
 
 @Service
@@ -19,12 +14,12 @@ class VisitSlotsNomisApiService(@Qualifier("nomisApiWebClient") private val webC
   private val api = VisitsConfigurationResourceApi(webClient)
 
   suspend fun getVisitTimeSlotIds(
-    pageNumber: Int = 0,
-    pageSize: Int = 1,
-  ): PageImpl<VisitTimeSlotIdResponse> = api.getVisitTimeSlotIds(
-    page = pageNumber,
-    size = pageSize,
-  ).awaitSingle().asPageImpl()
+    pageNumber: Long = 0,
+    pageSize: Long = 1,
+  ): PagedModelVisitTimeSlotIdResponse = api.getVisitTimeSlotIds(
+    page = pageNumber.toInt(),
+    size = pageSize.toInt(),
+  ).awaitSingle()
 
   suspend fun getVisitTimeSlot(
     prisonId: String,
@@ -36,7 +31,3 @@ class VisitSlotsNomisApiService(@Qualifier("nomisApiWebClient") private val webC
     timeSlotSequence = timeSlotSequence,
   ).awaitSingle()
 }
-
-fun PagedModelVisitTimeSlotIdResponse.asPageImpl(): PageImpl<VisitTimeSlotIdResponse> = PageImpl(this.content!!.toMutableList(), this.page!!.asPageable(), this.page.totalElements!!)
-
-fun PageMetadata.asPageable(): Pageable = PageRequest.of(this.number!!.toInt(), this.propertySize!!.toInt())
