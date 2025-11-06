3807c5aa78890281cdf0095825956b9fc2cd239d
andymarke <andy.marke@digital.justice.gov.uk>
2025-11-06 14:27:35 +0000

    SDIT-3143 Visit slot migration - audit fields and changed enum

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/officialvisits/VisitSlotsMappingService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/officialvisits/VisitSlotsMappingService.kt
@@ -11,17 +11,16 @@ import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomismappings.api
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomismappings.model.LocationMappingDto
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomismappings.model.VisitTimeSlotMappingDto
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomismappings.model.VisitTimeSlotMigrationMappingDto
-import java.time.DayOfWeek
 
 @Service
 class VisitSlotsMappingService(@Qualifier("mappingApiWebClient") webClient: WebClient) : MigrationMapping<VisitTimeSlotMigrationMappingDto>("/mapping/visit-slots", webClient) {
   private val api = VisitSlotsResourceApi(webClient)
   private val locationApi = LocationMappingResourceApi(webClient)
 
-  suspend fun getByNomisIdsOrNull(nomisPrisonId: String, nomisDayOfWeek: DayOfWeek, nomisSlotSequence: Int): VisitTimeSlotMappingDto? = api.prepare(
+  suspend fun getByNomisIdsOrNull(nomisPrisonId: String, nomisDayOfWeek: String, nomisSlotSequence: Int): VisitTimeSlotMappingDto? = api.prepare(
     api.getVisitTimeSlotMappingByNomisIdsRequestConfig(
       nomisPrisonId = nomisPrisonId,
-      nomisDayOfWeek = nomisDayOfWeek.asMappingDayOfWeek(),
+      nomisDayOfWeek = nomisDayOfWeek,
       nomisSlotSequence = nomisSlotSequence,
     ),
   )
@@ -29,14 +28,4 @@ class VisitSlotsMappingService(@Qualifier("mappingApiWebClient") webClient: WebC
     .awaitBodyOrNullWhenNotFound()
 
   suspend fun getInternalLocationByNomisId(nomisLocationId: Long): LocationMappingDto = locationApi.getMappingGivenNomisId1(nomisLocationId = nomisLocationId).awaitSingle()
-}
-
-private fun DayOfWeek.asMappingDayOfWeek(): VisitSlotsResourceApi.NomisDayOfWeekGetVisitTimeSlotMappingByNomisIds = when (this) {
-  DayOfWeek.MONDAY -> VisitSlotsResourceApi.NomisDayOfWeekGetVisitTimeSlotMappingByNomisIds.MONDAY
-  DayOfWeek.TUESDAY -> VisitSlotsResourceApi.NomisDayOfWeekGetVisitTimeSlotMappingByNomisIds.TUESDAY
-  DayOfWeek.WEDNESDAY -> VisitSlotsResourceApi.NomisDayOfWeekGetVisitTimeSlotMappingByNomisIds.WEDNESDAY
-  DayOfWeek.THURSDAY -> VisitSlotsResourceApi.NomisDayOfWeekGetVisitTimeSlotMappingByNomisIds.THURSDAY
-  DayOfWeek.FRIDAY -> VisitSlotsResourceApi.NomisDayOfWeekGetVisitTimeSlotMappingByNomisIds.FRIDAY
-  DayOfWeek.SATURDAY -> VisitSlotsResourceApi.NomisDayOfWeekGetVisitTimeSlotMappingByNomisIds.SATURDAY
-  DayOfWeek.SUNDAY -> VisitSlotsResourceApi.NomisDayOfWeekGetVisitTimeSlotMappingByNomisIds.SUNDAY
 }
