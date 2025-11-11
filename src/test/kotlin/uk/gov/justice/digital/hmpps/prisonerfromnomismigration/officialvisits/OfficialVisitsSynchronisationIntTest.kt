ba926d624846fa810683c8d322ca0393a603994a
Andy Marke <andy.marke@digital.justice.gov.uk>
2025-11-11 11:45:53 +0000

    SDIT-3100 Visit visitor xtag events (#1970)

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/officialvisits/OfficialVisitsSynchronisationIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/officialvisits/OfficialVisitsSynchronisationIntTest.kt
@@ -17,6 +17,8 @@ class OfficialVisitsSynchronisationIntTest : SqsIntegrationTestBase() {
   val bookingId = 1234L
   val prisonId = "MDI"
   val nomisVisitId = 65432L
+  val nomisVisitorId = 76544L
+  val nomisPersonId = 8987756L
 
   @Nested
   @DisplayName("OFFENDER_OFFICIAL_VISIT-INSERTED")
@@ -122,6 +124,207 @@ class OfficialVisitsSynchronisationIntTest : SqsIntegrationTestBase() {
       }
     }
   }
+
+  @Nested
+  @DisplayName("OFFENDER_OFFICIAL_VISIT_VISITORS-INSERTED")
+  inner class OfficialVisitVisitorCreated {
+
+    @Nested
+    inner class WhenCreatedInNomis {
+      @BeforeEach
+      fun setUp() {
+        officialVisitsOffenderEventsQueue.sendMessage(
+          officialVisitVisitorEvent(
+            eventType = "OFFENDER_OFFICIAL_VISIT_VISITORS-INSERTED",
+            offenderNo = offenderNo,
+            visitId = nomisVisitId,
+            bookingId = bookingId,
+            visitVisitorId = nomisVisitorId,
+            personId = nomisPersonId,
+          ),
+        ).also { waitForAnyProcessingToComplete() }
+      }
+
+      @Test
+      fun `will track telemetry`() {
+        verify(telemetryClient).trackEvent(
+          eq("officialvisits-visitor-synchronisation-created-success"),
+          check {
+            assertThat(it["offenderNo"]).isEqualTo(offenderNo)
+            assertThat(it["nomisVisitId"]).isEqualTo(nomisVisitId.toString())
+            assertThat(it["bookingId"]).isEqualTo(bookingId.toString())
+            assertThat(it["nomisVisitorId"]).isEqualTo(nomisVisitorId.toString())
+            assertThat(it["nomisPersonId"]).isEqualTo(nomisPersonId.toString())
+          },
+          isNull(),
+        )
+      }
+    }
+
+    @Nested
+    inner class WhenPrisonerVisitorCreatedInNomis {
+      @BeforeEach
+      fun setUp() {
+        officialVisitsOffenderEventsQueue.sendMessage(
+          officialVisitVisitorEvent(
+            eventType = "OFFENDER_OFFICIAL_VISIT_VISITORS-INSERTED",
+            offenderNo = offenderNo,
+            visitId = nomisVisitId,
+            bookingId = bookingId,
+            visitVisitorId = nomisVisitorId,
+          ),
+        ).also { waitForAnyProcessingToComplete() }
+      }
+
+      @Test
+      fun `will track telemetry`() {
+        verify(telemetryClient).trackEvent(
+          eq("officialvisits-visitor-synchronisation-created-ignored"),
+          check {
+            assertThat(it["offenderNo"]).isEqualTo(offenderNo)
+            assertThat(it["nomisVisitId"]).isEqualTo(nomisVisitId.toString())
+            assertThat(it["bookingId"]).isEqualTo(bookingId.toString())
+            assertThat(it["nomisVisitorId"]).isEqualTo(nomisVisitorId.toString())
+          },
+          isNull(),
+        )
+      }
+    }
+  }
+
+  @Nested
+  @DisplayName("OFFENDER_OFFICIAL_VISIT_VISITORS-UPDATED")
+  inner class OfficialVisitVisitorUpdated {
+
+    @Nested
+    inner class WhenUpdatedInNomis {
+      @BeforeEach
+      fun setUp() {
+        officialVisitsOffenderEventsQueue.sendMessage(
+          officialVisitVisitorEvent(
+            eventType = "OFFENDER_OFFICIAL_VISIT_VISITORS-UPDATED",
+            offenderNo = offenderNo,
+            visitId = nomisVisitId,
+            bookingId = bookingId,
+            visitVisitorId = nomisVisitorId,
+            personId = nomisPersonId,
+          ),
+        ).also { waitForAnyProcessingToComplete() }
+      }
+
+      @Test
+      fun `will track telemetry`() {
+        verify(telemetryClient).trackEvent(
+          eq("officialvisits-visitor-synchronisation-updated-success"),
+          check {
+            assertThat(it["offenderNo"]).isEqualTo(offenderNo)
+            assertThat(it["nomisVisitId"]).isEqualTo(nomisVisitId.toString())
+            assertThat(it["bookingId"]).isEqualTo(bookingId.toString())
+            assertThat(it["nomisVisitorId"]).isEqualTo(nomisVisitorId.toString())
+            assertThat(it["nomisPersonId"]).isEqualTo(nomisPersonId.toString())
+          },
+          isNull(),
+        )
+      }
+    }
+
+    @Nested
+    inner class WhenPrisonerVisitorUpdatedInNomis {
+      @BeforeEach
+      fun setUp() {
+        officialVisitsOffenderEventsQueue.sendMessage(
+          officialVisitVisitorEvent(
+            eventType = "OFFENDER_OFFICIAL_VISIT_VISITORS-UPDATED",
+            offenderNo = offenderNo,
+            visitId = nomisVisitId,
+            bookingId = bookingId,
+            visitVisitorId = nomisVisitorId,
+          ),
+        ).also { waitForAnyProcessingToComplete() }
+      }
+
+      @Test
+      fun `will track telemetry`() {
+        verify(telemetryClient).trackEvent(
+          eq("officialvisits-visitor-synchronisation-updated-ignored"),
+          check {
+            assertThat(it["offenderNo"]).isEqualTo(offenderNo)
+            assertThat(it["nomisVisitId"]).isEqualTo(nomisVisitId.toString())
+            assertThat(it["bookingId"]).isEqualTo(bookingId.toString())
+            assertThat(it["nomisVisitorId"]).isEqualTo(nomisVisitorId.toString())
+          },
+          isNull(),
+        )
+      }
+    }
+  }
+
+  @Nested
+  @DisplayName("OFFENDER_OFFICIAL_VISIT_VISITORS-DELETED")
+  inner class OfficialVisitVisitorDeleted {
+
+    @Nested
+    inner class WhenDeletedFromNomis {
+      @BeforeEach
+      fun setUp() {
+        officialVisitsOffenderEventsQueue.sendMessage(
+          officialVisitVisitorEvent(
+            eventType = "OFFENDER_OFFICIAL_VISIT_VISITORS-DELETED",
+            offenderNo = offenderNo,
+            visitId = nomisVisitId,
+            bookingId = bookingId,
+            visitVisitorId = nomisVisitorId,
+            personId = nomisPersonId,
+          ),
+        ).also { waitForAnyProcessingToComplete() }
+      }
+
+      @Test
+      fun `will track telemetry`() {
+        verify(telemetryClient).trackEvent(
+          eq("officialvisits-visitor-synchronisation-deleted-success"),
+          check {
+            assertThat(it["offenderNo"]).isEqualTo(offenderNo)
+            assertThat(it["nomisVisitId"]).isEqualTo(nomisVisitId.toString())
+            assertThat(it["bookingId"]).isEqualTo(bookingId.toString())
+            assertThat(it["nomisVisitorId"]).isEqualTo(nomisVisitorId.toString())
+            assertThat(it["nomisPersonId"]).isEqualTo(nomisPersonId.toString())
+          },
+          isNull(),
+        )
+      }
+    }
+
+    @Nested
+    inner class WhenPrisonerVisitorDeletedFromNomis {
+      @BeforeEach
+      fun setUp() {
+        officialVisitsOffenderEventsQueue.sendMessage(
+          officialVisitVisitorEvent(
+            eventType = "OFFENDER_OFFICIAL_VISIT_VISITORS-DELETED",
+            offenderNo = offenderNo,
+            visitId = nomisVisitId,
+            bookingId = bookingId,
+            visitVisitorId = nomisVisitorId,
+          ),
+        ).also { waitForAnyProcessingToComplete() }
+      }
+
+      @Test
+      fun `will track telemetry`() {
+        verify(telemetryClient).trackEvent(
+          eq("officialvisits-visitor-synchronisation-deleted-ignored"),
+          check {
+            assertThat(it["offenderNo"]).isEqualTo(offenderNo)
+            assertThat(it["nomisVisitId"]).isEqualTo(nomisVisitId.toString())
+            assertThat(it["bookingId"]).isEqualTo(bookingId.toString())
+            assertThat(it["nomisVisitorId"]).isEqualTo(nomisVisitorId.toString())
+          },
+          isNull(),
+        )
+      }
+    }
+  }
 }
 
 fun officialVisitEvent(
@@ -135,6 +338,49 @@ fun officialVisitEvent(
   """{
     "MessageId": "ae06c49e-1f41-4b9f-b2f2-dcca610d02cd", "Type": "Notification", "Timestamp": "2019-10-21T14:01:18.500Z", 
     "Message": "{\"eventType\":\"$eventType\",\"eventDatetime\":\"2019-10-21T15:00:25.489964\",\"offenderIdDisplay\": \"$offenderNo\", \"bookingId\": $bookingId, \"visitId\": $visitId,\"auditModuleName\":\"$auditModuleName\",\"agencyLocationId\": \"$agencyLocationId\",\"nomisEventType\":\"$eventType\" }",
+    "TopicArn": "arn:aws:sns:eu-west-1:000000000000:offender_events", 
+    "MessageAttributes": {
+      "eventType": {"Type": "String", "Value": "$eventType"}, 
+      "id": {"Type": "String", "Value": "8b07cbd9-0820-0a0f-c32f-a9429b618e0b"}, 
+      "contentType": {"Type": "String", "Value": "text/plain;charset=UTF-8"}, 
+      "timestamp": {"Type": "Number.java.lang.Long", "Value": "1571666478344"}
+    }
+}
+  """.trimIndent()
+
+fun officialVisitVisitorEvent(
+  eventType: String,
+  visitVisitorId: Long,
+  visitId: Long,
+  offenderNo: String,
+  bookingId: Long,
+  auditModuleName: String = "OIDUVISI",
+) = // language=JSON
+  """{
+    "MessageId": "ae06c49e-1f41-4b9f-b2f2-dcca610d02cd", "Type": "Notification", "Timestamp": "2019-10-21T14:01:18.500Z", 
+    "Message": "{\"eventType\":\"$eventType\",\"eventDatetime\":\"2019-10-21T15:00:25.489964\",\"offenderIdDisplay\": \"$offenderNo\", \"bookingId\": $bookingId, \"visitId\": $visitId,\"auditModuleName\":\"$auditModuleName\",\"visitVisitorId\": $visitVisitorId,\"nomisEventType\":\"$eventType\" }",
+    "TopicArn": "arn:aws:sns:eu-west-1:000000000000:offender_events", 
+    "MessageAttributes": {
+      "eventType": {"Type": "String", "Value": "$eventType"}, 
+      "id": {"Type": "String", "Value": "8b07cbd9-0820-0a0f-c32f-a9429b618e0b"}, 
+      "contentType": {"Type": "String", "Value": "text/plain;charset=UTF-8"}, 
+      "timestamp": {"Type": "Number.java.lang.Long", "Value": "1571666478344"}
+    }
+}
+  """.trimIndent()
+
+fun officialVisitVisitorEvent(
+  eventType: String,
+  visitVisitorId: Long,
+  visitId: Long,
+  offenderNo: String,
+  personId: Long,
+  bookingId: Long,
+  auditModuleName: String = "OIDUVISI",
+) = // language=JSON
+  """{
+    "MessageId": "ae06c49e-1f41-4b9f-b2f2-dcca610d02cd", "Type": "Notification", "Timestamp": "2019-10-21T14:01:18.500Z", 
+    "Message": "{\"eventType\":\"$eventType\",\"eventDatetime\":\"2019-10-21T15:00:25.489964\",\"offenderIdDisplay\": \"$offenderNo\", \"bookingId\": $bookingId, \"personId\": $personId, \"visitId\": $visitId,\"auditModuleName\":\"$auditModuleName\",\"visitVisitorId\": $visitVisitorId,\"nomisEventType\":\"$eventType\" }",
     "TopicArn": "arn:aws:sns:eu-west-1:000000000000:offender_events", 
     "MessageAttributes": {
       "eventType": {"Type": "String", "Value": "$eventType"}, 
