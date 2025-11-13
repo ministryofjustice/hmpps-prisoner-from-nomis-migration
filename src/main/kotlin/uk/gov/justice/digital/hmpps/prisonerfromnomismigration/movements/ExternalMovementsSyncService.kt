25bbb2c8be51564ab9c03ba8d3d87b0b6d325084
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2025-11-13 17:11:39 +0000

    Revert "SDIT-2868 Add logging to assist debugging (#1980)" (#1982)
    This reverts commit 72ef99dff6a891dd1e92d09ef7ebf73aa6290335.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/movements/ExternalMovementsSyncService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/movements/ExternalMovementsSyncService.kt
@@ -792,9 +792,6 @@ fun ScheduledTemporaryAbsenceResponse.toDpsRequest(id: UUID? = null) = SyncWrite
   updated = audit.modifyDatetime?.let { SyncAtAndBy(at = audit.modifyDatetime, by = audit.modifyUserId!!) },
   legacyId = eventId,
 )
-  .also { log.info("Syncing eventId=${it.legacyId}, eventStatus=$eventStatus, inboundEventStatus=$inboundEventStatus, returnTime=$returnTime, dps_status=${it.statusCode}") }
-
-private val log = LoggerFactory.getLogger(ExternalMovementsSyncService::class.java)
 
 private fun String.toDpsOccurrenceStatusCode(inboundStatus: String? = null, returnBy: LocalDateTime) = when (this) {
   "CANC" -> "CANCELLED"
