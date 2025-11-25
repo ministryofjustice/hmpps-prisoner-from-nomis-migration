f2ca1b50c80165ffcf4174733bf081ba4d1a30d1
petergphillips <5099053+petergphillips@users.noreply.github.com>
2025-11-25 11:51:19 +0000

    SDIT-3216: 💡 Improve insights version comment (#2044)

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,9 +42,11 @@ dependencies {
   runtimeOnly("org.springframework.boot:spring-boot-starter-jdbc")
   runtimeOnly("org.postgresql:postgresql:42.7.8")
   runtimeOnly("org.flywaydb:flyway-database-postgresql")
-  // Should match the version used in App Insights pulled in by the plugin - https://github.com/ministryofjustice/hmpps-gradle-spring-boot/blob/main/src/main/kotlin/uk/gov/justice/digital/hmpps/gradle/configmanagers/AppInsightsConfigManager.kt#L10
-  implementation("io.opentelemetry:opentelemetry-extension-kotlin:1.53.0")
+  // Needs to match this version https://github.com/microsoft/ApplicationInsights-Java/blob/<version>/dependencyManagement/build.gradle.kts#L16
+  // where <version> is the version of application insights pulled in by hmpps-gradle-spring-boot
+  // at https://github.com/ministryofjustice/hmpps-gradle-spring-boot/blob/main/src/main/kotlin/uk/gov/justice/digital/hmpps/gradle/configmanagers/AppInsightsConfigManager.kt#L7
   implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.19.0")
+  implementation("io.opentelemetry:opentelemetry-extension-kotlin:1.53.0")
 
   testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.8.2")
   testImplementation("io.swagger.parser.v3:swagger-parser:2.1.35") {
