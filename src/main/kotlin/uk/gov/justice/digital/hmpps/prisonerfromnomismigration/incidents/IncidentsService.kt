cc51778bbe7da059a1d30dbd71b68555ee2aedec
karenmillermoj <karen.miller3@justice.gov.uk>
2026-01-23 14:40:45 +0000

    SDIT-3315 Update incidents api docs

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/incidents/IncidentsService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/incidents/IncidentsService.kt
@@ -6,12 +6,15 @@ import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.awaitBody
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.helpers.awaitBodyOrLogAndRethrowBadRequest
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.helpers.awaitBodyOrNullWhenNotFound
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.incidents.api.IncidentReportsApi
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.incidents.model.NomisSyncReportId
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.incidents.model.NomisSyncRequest
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.incidents.model.ReportBasic
+import java.util.UUID
 
 @Service
 class IncidentsService(@Qualifier("incidentsApiWebClient") private val webClient: WebClient) {
+  private val incidentsApi = IncidentReportsApi(webClient)
 
   suspend fun upsertIncident(syncRequest: NomisSyncRequest): NomisSyncReportId = webClient.post()
     .uri("/sync/upsert")
@@ -19,13 +22,13 @@ class IncidentsService(@Qualifier("incidentsApiWebClient") private val webClient
     .retrieve()
     .awaitBodyOrLogAndRethrowBadRequest()
 
-  suspend fun deleteIncident(incidentId: String) = webClient.delete()
-    .uri("/incident-reports/{incidentId}", incidentId)
+  suspend fun deleteIncident(incidentId: String) = incidentsApi
+    .prepare(incidentsApi.deleteReportRequestConfig(UUID.fromString(incidentId)))
     .retrieve()
     .awaitBodyOrNullWhenNotFound<Unit>()
 
-  suspend fun getIncidentByNomisId(nomisIncidentId: Long): ReportBasic = webClient.get()
-    .uri("/incident-reports/reference/{nomisIncidentId}", nomisIncidentId)
+  suspend fun getIncidentByNomisId(nomisIncidentId: Long): ReportBasic = incidentsApi
+    .prepare(incidentsApi.getBasicReportByReferenceRequestConfig(nomisIncidentId.toString()))
     .retrieve()
     .awaitBody()
 }
