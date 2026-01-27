749f0e728bf22fae81d0682d7059e814ac2159fe
lbennett-moj <lucy.bennett@digital .justice.gov.uk>
2026-01-27 17:12:49 +0000

    sdit-3340 add offence code to logs

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/courtsentencing/CourtSentencingSynchronisationIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/courtsentencing/CourtSentencingSynchronisationIntTest.kt
@@ -3962,7 +3962,6 @@ class CourtSentencingSynchronisationIntTest : SqsIntegrationTestBase() {
               assertThat(it["offenderNo"]).isEqualTo(OFFENDER_ID_DISPLAY)
               assertThat(it["nomisBookingId"]).isEqualTo(NOMIS_BOOKING_ID.toString())
               assertThat(it["reason"]).isEqualTo("charge is not mapped")
-              assertThat(it["offenceCode"]).isEqualTo("TTT4006")
               assertThat(it["nomisOffenderChargeId"]).isEqualTo(NOMIS_OFFENDER_CHARGE_ID.toString())
             },
             isNull(),
@@ -4022,6 +4021,7 @@ class CourtSentencingSynchronisationIntTest : SqsIntegrationTestBase() {
               assertThat(it["nomisBookingId"]).isEqualTo(NOMIS_BOOKING_ID.toString())
               assertThat(it["nomisOffenderChargeId"]).isEqualTo(NOMIS_OFFENDER_CHARGE_ID.toString())
               assertThat(it["dpsChargeId"]).isEqualTo(DPS_CHARGE_ID)
+              assertThat(it["offenceCode"]).isEqualTo("TTT4006")
             },
             isNull(),
           )
