954b1c834aec7159d733b1fe12feaa6ac4420ac2
Karen Miller <75248289+karenmillermoj@users.noreply.github.com>
2026-02-09 10:03:21 +0000

    SDIT-2358 Pick up latest nomis-prisone-api docs (#2392)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/finance/FinanceNomisApiService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/finance/FinanceNomisApiService.kt
@@ -5,21 +5,23 @@ import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomisprisoner.api.PrisonBalanceResourceApi
-import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomisprisoner.api.TransactionsResourceApi
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomisprisoner.api.PrisonTransactionsResourceApi
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomisprisoner.api.PrisonerTransactionsResourceApi
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomisprisoner.model.GeneralLedgerTransactionDto
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomisprisoner.model.OffenderTransactionDto
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomisprisoner.model.PrisonBalanceDto
 
 @Service
 class FinanceNomisApiService(@Qualifier("nomisApiWebClient") webClient: WebClient) {
-  private val transactionsApi = TransactionsResourceApi(webClient)
+  private val prisonerTransactionsApi = PrisonerTransactionsResourceApi(webClient)
+  private val prisonTransactionsApi = PrisonTransactionsResourceApi(webClient)
   private val prisonApi = PrisonBalanceResourceApi(webClient)
 
-  suspend fun getPrisonerTransactions(transactionId: Long): List<OffenderTransactionDto> = transactionsApi
+  suspend fun getPrisonerTransactions(transactionId: Long): List<OffenderTransactionDto> = prisonerTransactionsApi
     .getTransaction(transactionId)
     .awaitSingle()
 
-  suspend fun getGLTransactions(transactionId: Long): List<GeneralLedgerTransactionDto> = transactionsApi
+  suspend fun getGLTransactions(transactionId: Long): List<GeneralLedgerTransactionDto> = prisonTransactionsApi
     .getGLTransaction(transactionId)
     .awaitSingle()
 
