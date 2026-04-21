fbdc3bfb27b523e837628719b65cf3fd119dc759
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2026-04-21 08:38:18 +0100

    SDIT-2832 Switch get tap movements to new NOMIS API (#2716)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/movements/taps/TapsNomisApiService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/movements/taps/TapsNomisApiService.kt
@@ -4,22 +4,22 @@ import kotlinx.coroutines.reactive.awaitSingle
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
-import org.springframework.web.reactive.function.client.awaitBody
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.helpers.awaitBodyOrNullWhenNotFound
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomisprisoner.api.OffenderTapsResourceApi
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomisprisoner.api.TapApplicationResourceApi
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomisprisoner.api.TapMovementResourceApi
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomisprisoner.api.TapScheduleResourceApi
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomisprisoner.model.OffenderTapsResponse
-import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomisprisoner.model.ScheduledTemporaryAbsenceReturnResponse
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomisprisoner.model.TapApplication
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomisprisoner.model.TapMovementIn
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomisprisoner.model.TapMovementOut
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomisprisoner.model.TapScheduleOut
-import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomisprisoner.model.TemporaryAbsenceResponse
-import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomisprisoner.model.TemporaryAbsenceReturnResponse
 
 @Service
 class TapsNomisApiService(@Qualifier("nomisApiWebClient") private val webClient: WebClient) {
 
   private val applicationApi = TapApplicationResourceApi(webClient)
+  private val movementApi = TapMovementResourceApi(webClient)
   private val offenderApi = OffenderTapsResourceApi(webClient)
   private val scheduleApi = TapScheduleResourceApi(webClient)
 
@@ -33,27 +33,9 @@ class TapsNomisApiService(@Qualifier("nomisApiWebClient") private val webClient:
   suspend fun getTapScheduleOut(offenderNo: String, eventId: Long): TapScheduleOut = scheduleApi.getTapScheduleOut(offenderNo, eventId)
     .awaitSingle()
 
-  suspend fun getTemporaryAbsenceScheduledReturnMovement(offenderNo: String, eventId: Long) = webClient.get()
-    .uri {
-      it.path("/movements/{offenderNo}/temporary-absences/scheduled-temporary-absence-return/{eventId}")
-        .build(offenderNo, eventId)
-    }
-    .retrieve()
-    .awaitBody<ScheduledTemporaryAbsenceReturnResponse>()
+  suspend fun getTapMovementOut(offenderNo: String, bookingId: Long, movementSeq: Int): TapMovementOut = movementApi.getTapMovementOut(offenderNo, bookingId, movementSeq)
+    .awaitSingle()
 
-  suspend fun getTemporaryAbsenceMovement(offenderNo: String, bookingId: Long, movementSeq: Int) = webClient.get()
-    .uri {
-      it.path("/movements/{offenderNo}/temporary-absences/temporary-absence/{bookingId}/{movementSeq}")
-        .build(offenderNo, bookingId, movementSeq)
-    }
-    .retrieve()
-    .awaitBody<TemporaryAbsenceResponse>()
-
-  suspend fun getTemporaryAbsenceReturnMovement(offenderNo: String, bookingId: Long, movementSeq: Int) = webClient.get()
-    .uri {
-      it.path("/movements/{offenderNo}/temporary-absences/temporary-absence-return/{bookingId}/{movementSeq}")
-        .build(offenderNo, bookingId, movementSeq)
-    }
-    .retrieve()
-    .awaitBody<TemporaryAbsenceReturnResponse>()
+  suspend fun getTapMovementIn(offenderNo: String, bookingId: Long, movementSeq: Int): TapMovementIn = movementApi.getTapMovementIn(offenderNo, bookingId, movementSeq)
+    .awaitSingle()
 }
