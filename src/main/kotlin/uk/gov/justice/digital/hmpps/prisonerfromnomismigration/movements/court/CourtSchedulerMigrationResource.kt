cabfbedbc2c3d2efd48fa8f4872486f92d9573b5
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2026-05-19 12:39:06 +0100

    SDIT-3731 Add endpoint for court movement migrate / repair (#2856)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/movements/court/CourtSchedulerMigrationResource.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/movements/court/CourtSchedulerMigrationResource.kt
@@ -10,7 +10,9 @@ import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
@@ -52,4 +54,44 @@ class CourtSchedulerMigrationResource(
   suspend fun migrateCourtMovements(
     @RequestBody @Valid migrationFilter: CourtSchedulerMigrationFilter,
   ) = migrationService.startMigration(migrationFilter)
+
+  @PreAuthorize("hasAnyRole('ROLE_PRISONER_FROM_NOMIS__MIGRATION__RW','ROLE_PRISONER_FROM_NOMIS__REPAIR_MOVEMENTS__RW')")
+  @PutMapping("/repair/{prisonerNumber}")
+  @Operation(
+    summary = "Repair all court movements for a single prisoner",
+    description = "Resyncs a single prisoner to DPS. For prisoners with lots of movements this could be a lengthy process - maybe up to 2 minutes.",
+    responses = [
+      ApiResponse(
+        responseCode = "200",
+        description = "Prisoner re-synchronised",
+      ),
+      ApiResponse(
+        responseCode = "401",
+        description = "Unauthorized to access this endpoint",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "403",
+        description = "Incorrect permissions to start migration",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "404",
+        description = "Prisoner not found in NOMIS",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+    ],
+  )
+  suspend fun repairPrisonerCourtMovements(
+    @Schema(description = "The prisoner to resync")
+    @PathVariable prisonerNumber: String,
+  ) {
+    telemetryClient.trackEvent(
+      "court-movements-migration-entity-repair-requested",
+      mapOf("offenderNo" to prisonerNumber),
+      null,
+    )
+
+    migrationService.resyncPrisonerCourtMovements(prisonerNumber)
+  }
 }
