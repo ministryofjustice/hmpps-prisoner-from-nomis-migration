5b832378fbc067c381a8d6edee1a6cb834a70d9d
Mike Halma <mike.halma@digital.justice.gov.uk>
2026-05-20 09:22:04 +0100

    SDIT-3733 More tidying up language

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/movements/court/CourtSchedulerMigrationMessageListener.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/movements/court/CourtSchedulerMigrationMessageListener.kt
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service
 import software.amazon.awssdk.services.sqs.model.Message
 import tools.jackson.databind.json.JsonMapper
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.listeners.MigrationMessageListener
-import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.service.COURT_MOVEMENTS_QUEUE_ID
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.service.COURT_SCHEDULER_QUEUE_ID
 import java.util.concurrent.CompletableFuture
 
 @Service
@@ -18,7 +18,7 @@ class CourtSchedulerMigrationMessageListener(
 ) {
 
   @SqsListener(
-    COURT_MOVEMENTS_QUEUE_ID,
+    COURT_SCHEDULER_QUEUE_ID,
     factory = "hmppsQueueContainerFactoryProxy",
     maxConcurrentMessages = "8",
     maxMessagesPerPoll = "8",
--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/integration/SqsIntegrationTestBase.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/integration/SqsIntegrationTestBase.kt
@@ -49,7 +49,7 @@ import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.service.ALLOCATIO
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.service.APPOINTMENTS_QUEUE_ID
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.service.CASENOTES_SYNC_QUEUE_ID
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.service.CORE_PERSON_SYNC_QUEUE_ID
-import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.service.COURTMOVMENTS_SYNC_QUEUE_ID
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.service.COURTSCHEDULER_SYNC_QUEUE_ID
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.service.COURT_SENTENCING_QUEUE_ID
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.service.COURT_SENTENCING_SYNC_QUEUE_ID
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.service.CSRA_QUEUE_ID
@@ -200,7 +200,7 @@ class SqsIntegrationTestBase : TestBase() {
   internal val externalMovementsQueueOffenderEventsUrl by lazy { externalMovementsOffenderEventsQueue.queueUrl }
   internal val externalMovementsQueueOffenderEventsDlqUrl by lazy { externalMovementsOffenderEventsQueue.dlqUrl as String }
 
-  internal val courtMovementsOffenderEventsQueue by lazy { hmppsQueueService.findByQueueId(COURTMOVMENTS_SYNC_QUEUE_ID) as HmppsQueue }
+  internal val courtMovementsOffenderEventsQueue by lazy { hmppsQueueService.findByQueueId(COURTSCHEDULER_SYNC_QUEUE_ID) as HmppsQueue }
   internal val awsSqsCourtMovementsOffenderEventsClient by lazy { courtMovementsOffenderEventsQueue.sqsClient }
   internal val awsSqsCourtMovementsOffenderEventsDlqClient by lazy { courtMovementsOffenderEventsQueue.sqsDlqClient as SqsAsyncClient }
   internal val courtMovementsQueueOffenderEventsUrl by lazy { courtMovementsOffenderEventsQueue.queueUrl }
