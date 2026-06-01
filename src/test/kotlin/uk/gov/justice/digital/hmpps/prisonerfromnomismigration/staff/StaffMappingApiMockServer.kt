7e06ecaaec9dbaa9a18064aaf0fe32023e32a255
Karen Miller <75248289+karenmillermoj@users.noreply.github.com>
2026-06-01 10:46:14 +0100

    SDIT-3768 Add mapping and tests for staff migration (#2899)

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/staff/StaffMappingApiMockServer.kt
@@ -0,0 +1,81 @@
+package uk.gov.justice.digital.hmpps.prisonerfromnomismigration.staff
+
+import com.github.tomakehurst.wiremock.client.CountMatchingStrategy
+import com.github.tomakehurst.wiremock.client.WireMock.aResponse
+import com.github.tomakehurst.wiremock.client.WireMock.get
+import com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo
+import com.github.tomakehurst.wiremock.client.WireMock.urlPathMatching
+import com.github.tomakehurst.wiremock.matching.RequestPatternBuilder
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Component
+import tools.jackson.databind.json.JsonMapper
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomismappings.model.ErrorResponse
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomismappings.model.StaffMappingDto
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.wiremock.MappingApiExtension.Companion.mappingApi
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.wiremock.pageContent
+import java.time.LocalDateTime
+
+@Component
+class StaffMappingApiMockServer(private val jsonMapper: JsonMapper) {
+
+  fun stubGetByNomisStaffIdOrNull(
+    nomisStaffId: Long = 1234L,
+    mapping: StaffMappingDto? = StaffMappingDto(
+      dpsId = "123456",
+      nomisId = nomisStaffId,
+      mappingType = StaffMappingDto.MappingType.MIGRATED,
+    ),
+  ) {
+    mapping?.apply {
+      mappingApi.stubFor(
+        get(urlEqualTo("/mapping/staff/nomis-id/$nomisStaffId")).willReturn(
+          aResponse()
+            .withHeader("Content-Type", "application/json")
+            .withStatus(HttpStatus.OK.value())
+            .withBody(jsonMapper.writeValueAsString(mapping)),
+        ),
+      )
+    } ?: run {
+      mappingApi.stubFor(
+        get(urlEqualTo("/mapping/staff/nomis-id/$nomisStaffId")).willReturn(
+          aResponse()
+            .withHeader("Content-Type", "application/json")
+            .withStatus(HttpStatus.NOT_FOUND.value())
+            .withBody(jsonMapper.writeValueAsString(ErrorResponse(status = 404))),
+        ),
+      )
+    }
+  }
+
+  fun stubGetMigrationCount(migrationId: String = "2020-01-01T11:10:00", count: Int = 1) {
+    mappingApi.stubFor(
+      get(urlPathMatching("/mapping/staff/migration-id/.*")).willReturn(
+        aResponse()
+          .withHeader("Content-Type", "application/json")
+          .withBody(
+            pageContent(
+              jsonMapper = jsonMapper,
+              content = listOf(
+                StaffMappingDto(
+                  nomisId = 1234,
+                  dpsId = "654321",
+                  label = migrationId,
+                  whenCreated = LocalDateTime.now().toString(),
+                  mappingType = StaffMappingDto.MappingType.MIGRATED,
+                ),
+              ),
+              pageSize = 1L,
+              pageNumber = 0L,
+              totalElements = count.toLong(),
+              size = 1,
+            ),
+          ),
+      ),
+    )
+  }
+
+  fun verify(pattern: RequestPatternBuilder) = mappingApi.verify(pattern)
+  fun verify(count: Int, pattern: RequestPatternBuilder) = mappingApi.verify(count, pattern)
+  fun verify(count: CountMatchingStrategy, pattern: RequestPatternBuilder) = mappingApi.verify(count, pattern)
+  fun resetAll() = mappingApi.resetAll()
+}
---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/staff/StaffMappingApiServiceTest.kt
@@ -0,0 +1,137 @@
+package uk.gov.justice.digital.hmpps.prisonerfromnomismigration.staff
+
+import com.github.tomakehurst.wiremock.client.WireMock.aResponse
+import com.github.tomakehurst.wiremock.client.WireMock.anyUrl
+import com.github.tomakehurst.wiremock.client.WireMock.equalTo
+import com.github.tomakehurst.wiremock.client.WireMock.get
+import com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor
+import com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo
+import com.github.tomakehurst.wiremock.client.WireMock.urlPathMatching
+import kotlinx.coroutines.test.runTest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Import
+import org.springframework.http.HttpStatus
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.helper.SpringAPIServiceTest
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomismappings.model.StaffMappingDto
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.wiremock.MappingApiExtension.Companion.mappingApi
+
+@SpringAPIServiceTest
+@Import(StaffMappingApiService::class, StaffMappingApiMockServer::class)
+class StaffMappingApiServiceTest {
+
+  @Autowired
+  private lateinit var apiService: StaffMappingApiService
+
+  @Autowired
+  private lateinit var mockServer: StaffMappingApiMockServer
+
+  @Nested
+  inner class GetByNomisStaffIdOrNull {
+    val nomisStaffId = 12345L
+
+    @Test
+    fun `will pass oath2 token to service`() = runTest {
+      mockServer.stubGetByNomisStaffIdOrNull(
+        nomisStaffId = nomisStaffId,
+        mapping = StaffMappingDto(
+          dpsId = "1234",
+          nomisId = nomisStaffId,
+          mappingType = StaffMappingDto.MappingType.MIGRATED,
+        ),
+      )
+
+      apiService.getByNomisStaffIdOrNull(nomisStaffId = nomisStaffId)
+
+      mockServer.verify(
+        getRequestedFor(anyUrl()).withHeader("Authorization", equalTo("Bearer ABCDE")),
+      )
+    }
+
+    @Test
+    fun `will pass NOMIS id to service`() = runTest {
+      mockServer.stubGetByNomisStaffIdOrNull(
+        nomisStaffId = nomisStaffId,
+        mapping = StaffMappingDto(
+          dpsId = "1234",
+          nomisId = nomisStaffId,
+          mappingType = StaffMappingDto.MappingType.MIGRATED,
+        ),
+      )
+
+      apiService.getByNomisStaffIdOrNull(nomisStaffId = nomisStaffId)
+
+      mockServer.verify(
+        getRequestedFor(urlPathEqualTo("/mapping/staff/nomis-id/$nomisStaffId")),
+      )
+    }
+
+    @Test
+    fun `will return dpsId when mapping exists`() = runTest {
+      mockServer.stubGetByNomisStaffIdOrNull(
+        nomisStaffId = nomisStaffId,
+        mapping = StaffMappingDto(
+          dpsId = "1234",
+          nomisId = nomisStaffId,
+          mappingType = StaffMappingDto.MappingType.MIGRATED,
+        ),
+      )
+
+      val mapping = apiService.getByNomisStaffIdOrNull(nomisStaffId = nomisStaffId)
+
+      assertThat(mapping?.dpsId).isEqualTo("1234")
+    }
+
+    @Test
+    fun `will return null if mapping does not exist`() = runTest {
+      mockServer.stubGetByNomisStaffIdOrNull(
+        nomisStaffId = nomisStaffId,
+        mapping = null,
+      )
+
+      assertThat(apiService.getByNomisStaffIdOrNull(nomisStaffId = nomisStaffId)).isNull()
+    }
+  }
+
+  @Nested
+  inner class GetMigrationCount {
+    @BeforeEach
+    fun setUp() {
+      mockServer.stubGetMigrationCount("2020-01-01T10:00:00", count = 56_766)
+    }
+
+    @Test
+    fun `will supply authentication token`(): Unit = runTest {
+      apiService.getMigrationCount("2020-01-01T10:00:00")
+
+      mappingApi.verify(
+        getRequestedFor(
+          urlPathMatching("/mapping/staff/migration-id/.*"),
+        )
+          .withHeader("Authorization", equalTo("Bearer ABCDE")),
+      )
+    }
+
+    @Test
+    fun `will return zero when not found`(): Unit = runTest {
+      mappingApi.stubFor(
+        get(urlPathMatching("/mapping/staff/migration-id/.*")).willReturn(
+          aResponse()
+            .withHeader("Content-Type", "application/json")
+            .withStatus(HttpStatus.NOT_FOUND.value())
+            .withBody("""{"message":"Not found"}"""),
+        ),
+      )
+
+      assertThat(apiService.getMigrationCount("2020-01-01T10:00:00")).isEqualTo(0)
+    }
+
+    @Test
+    fun `will return the mapping count when found`(): Unit = runTest {
+      assertThat(apiService.getMigrationCount("2020-01-01T10:00:00")).isEqualTo(56_766)
+    }
+  }
+}
