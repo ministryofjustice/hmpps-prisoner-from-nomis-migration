9150d9d8003cf0d5174b0c31e79ed99547803a9d
karenmillermoj <karen.miller3@justice.gov.uk>
2026-06-01 12:25:38 +0100

    SDIT-3722 Add Nomis getStaffIds and pull in latest nomis-prisoner-api docs

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/staff/StaffNomisApiService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/staff/StaffNomisApiService.kt
@@ -5,14 +5,18 @@ import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomisprisoner.api.StaffResourceApi
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomisprisoner.model.PagedModelStaffIdResponse
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomisprisoner.model.StaffDetails
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.nomisprisoner.model.StaffIdsPage
 
 @Service
-class StaffNomisApiService(@Qualifier("nomisApiWebClient") webClient: WebClient) {
+class StaffNomisApiService(@Qualifier("nomisApiWebClient") private val webClient: WebClient) {
   private val api = StaffResourceApi(webClient)
 
-  suspend fun getStaffDetails(staffId: Long, dpsRolesOnly: Boolean = true): StaffDetails = api.getUser(staffId, dpsRolesOnly)
+  suspend fun getStaffDetails(staffId: Long, dpsRolesOnly: Boolean = true): StaffDetails = api.getStaff(staffId, dpsRolesOnly)
+    .awaitSingle()
+
+  suspend fun getStaffIds(pageNumber: Long, pageSize: Long): PagedModelStaffIdResponse = api.getStaffIds(page = pageNumber.toInt(), size = pageSize.toInt())
     .awaitSingle()
 
   suspend fun getStaffIdsFromId(lastStaffId: Long = 0, pageSize: Long): StaffIdsPage = api.getStaffIdsFromId(staffId = lastStaffId, size = pageSize.toInt())
