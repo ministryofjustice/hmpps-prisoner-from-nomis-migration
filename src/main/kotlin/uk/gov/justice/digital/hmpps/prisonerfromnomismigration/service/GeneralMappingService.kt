575f347b2b6f82901c7200e192fa75c139af89f7
karenmillermoj <karen.miller3@justice.gov.uk>
2026-06-02 16:50:13 +0100

    SDIT-3679 Add Staff migration

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/service/GeneralMappingService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/service/GeneralMappingService.kt
@@ -13,6 +13,7 @@ import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.movements.court.C
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.movements.taps.TapMappingApiService
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.officialvisits.OfficialVisitsMappingService
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.officialvisits.VisitSlotsMappingService
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.staff.StaffMappingApiService
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.visits.VisitMappingService
 
 @Service
@@ -26,6 +27,7 @@ class GeneralMappingService(
   private val courtSentencingMappingService: CourtSentencingMappingApiService,
   private val prisonBalanceMappingApiService: PrisonBalanceMappingApiService,
   private val prisonerBalanceMappingApiService: PrisonerBalanceMappingApiService,
+  private val staffMappingApiService: StaffMappingApiService,
   private val tapMappingApiService: TapMappingApiService,
   private val visitSlotsMappingService: VisitSlotsMappingService,
   private val officialVisitsMappingService: OfficialVisitsMappingService,
@@ -43,6 +45,7 @@ class GeneralMappingService(
     MigrationType.OFFICIAL_VISITS -> officialVisitsMappingService.getMigrationCount(migrationId)
     MigrationType.PRISON_BALANCE -> prisonBalanceMappingApiService.getPagedModelMigrationCount(migrationId)
     MigrationType.PRISONER_BALANCE -> prisonerBalanceMappingApiService.getPagedModelMigrationCount(migrationId)
+    MigrationType.STAFF -> staffMappingApiService.getPagedModelMigrationCount(migrationId)
     MigrationType.VISIT_SLOTS -> visitSlotsMappingService.getMigrationCount(migrationId)
     MigrationType.VISITS -> visitMappingService.getMigrationCount(migrationId)
   }
---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/staff/StaffMigrationResource.kt
@@ -0,0 +1,81 @@
+package uk.gov.justice.digital.hmpps.prisonerfromnomismigration.staff
+
+import com.fasterxml.jackson.annotation.JsonInclude
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.validation.Valid
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.config.ErrorResponse
+import java.time.LocalDate
+
+@RestController
+@RequestMapping("/migrate/staff", produces = [MediaType.APPLICATION_JSON_VALUE])
+@Tag(name = "Staff Migration Resource")
+@PreAuthorize("hasRole('ROLE_PRISONER_FROM_NOMIS__MIGRATION__RW')")
+class StaffMigrationResource(
+  private val migrationService: StaffMigrationService,
+) {
+  @PostMapping
+  @ResponseStatus(value = HttpStatus.ACCEPTED)
+  @Operation(
+    summary = "Starts a staff migration. This migration currently does nothing with the filter",
+    description = """
+      Starts an asynchronous staff migration process. 
+      This operation will return immediately and the migration will be performed asynchronously.
+       Requires role <b>PRISONER_FROM_NOMIS__MIGRATION__RW</b>""",
+    requestBody = io.swagger.v3.oas.annotations.parameters.RequestBody(
+      content = [
+        Content(
+          mediaType = "application/json",
+          schema = Schema(implementation = StaffMigrationFilter::class),
+        ),
+      ],
+    ),
+    responses = [
+      ApiResponse(
+        responseCode = "202",
+        description = "Migration process started",
+      ),
+      ApiResponse(
+        responseCode = "401",
+        description = "Unauthorized to access this endpoint",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "403",
+        description = "Incorrect permissions to start migration",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "409",
+        description = "Migration already in progress",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+    ],
+  )
+  suspend fun startStaffMigration(
+    @RequestBody @Valid
+    migrationFilter: StaffMigrationFilter,
+  ) = migrationService.startMigration(migrationFilter)
+}
+
+// TODO - the filter currently does nothing - determine if we want any fields passed through
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Schema(description = "Filter specifying what should be migrated from NOMIS to DPS")
+data class StaffMigrationFilter(
+  @Schema(description = "Only include visits created after this date. NB this is creation date not the actual visit date", example = "2020-03-23")
+  val fromDate: LocalDate? = null,
+
+  @Schema(description = "Only include visits created before this date. NB this is creation date not the actual visit date", example = "2020-03-24")
+  val toDate: LocalDate? = null,
+)
