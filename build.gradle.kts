8fcfabab5b94b308c40354d52d910e6d8ff6e7a4
petergphillips <5099053+petergphillips@users.noreply.github.com>
2026-08-17 13:27:27 +0100

    SDIT-4153: 🔒️ Upgrade gradle plugin for httpclient and suppress dompurify (#3318)

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ import kotlin.io.path.pathString
 import kotlin.io.path.Path as KotlinPath
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "11.0.4"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "11.0.5"
   kotlin("plugin.spring") version "2.4.10"
   id("org.openapi.generator") version "7.23.0"
 }
@@ -69,6 +69,7 @@ kotlin {
   jvmToolchain(25)
   compilerOptions {
     freeCompilerArgs.add("-Xcollection-literals")
+    jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_25)
   }
 }
 
@@ -270,7 +271,6 @@ val models = listOf(
 tasks {
   withType<KotlinCompile> {
     dependsOn(models.map { it.toBuildModelTaskName() })
-    compilerOptions.jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_25
   }
   withType<KtLintCheckTask> {
     mustRunAfter(models.map { it.toBuildModelTaskName() })
