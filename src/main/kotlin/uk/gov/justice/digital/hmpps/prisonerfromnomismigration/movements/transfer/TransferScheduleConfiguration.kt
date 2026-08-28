f0e59ff79dbf60f076b22331341ee3526517a21b
Mike Halma <mike.halma@digital.justice.gov.uk>
2026-08-28 13:55:59 +0100

    SDIT-4063 Missing health checks

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/movements/transfer/TransferScheduleConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/movements/transfer/TransferScheduleConfiguration.kt
@@ -1,11 +1,15 @@
 package uk.gov.justice.digital.hmpps.prisonerfromnomismigration.movements.transfer
 
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientManager
+import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 import uk.gov.justice.hmpps.kotlin.auth.reactiveAuthorisedWebClient
+import uk.gov.justice.hmpps.kotlin.auth.reactiveHealthWebClient
+import uk.gov.justice.hmpps.kotlin.health.ReactiveHealthPingCheck
 import java.time.Duration
 
 @Configuration
@@ -25,4 +29,10 @@ class TransferScheduleConfiguration(
 
   @Bean
   fun transferScheduleMappingApiWebClient(authorizedClientManager: ReactiveOAuth2AuthorizedClientManager, builder: WebClient.Builder): WebClient = builder.reactiveAuthorisedWebClient(authorizedClientManager, registrationId = "nomis-mapping-api", url = mappingApiBaseUri, mappingTimeout)
+
+  @Bean
+  fun transferSchedulerApiHealthWebClient(builder: WebClient.Builder): WebClient = builder.reactiveHealthWebClient(transferSchedulerUrl, healthTimeout)
+
+  @Component("transferSchedulerApi")
+  class TransferSchedulerApiHealth(@Qualifier("transferSchedulerApiHealthWebClient") webClient: WebClient) : ReactiveHealthPingCheck(webClient)
 }
