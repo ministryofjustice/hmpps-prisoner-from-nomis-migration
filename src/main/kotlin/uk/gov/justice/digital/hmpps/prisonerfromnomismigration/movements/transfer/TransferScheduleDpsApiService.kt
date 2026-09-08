03914009b7114301ffd3f7296644a8625a7da87a
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2026-09-08 13:46:10 +0100

    SDIT-4117 Add client for DPS move booking endpoint (#3452)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/movements/transfer/TransferScheduleDpsApiService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfromnomismigration/movements/transfer/TransferScheduleDpsApiService.kt
@@ -4,9 +4,11 @@ import kotlinx.coroutines.reactor.awaitSingle
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.helpers.awaitBodilessEntityOrLogAndRethrowBadRequest
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.helpers.awaitBodyOrLogAndRethrowBadRequest
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.helpers.awaitBodyOrNullWhenNotFound
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.transferschedule.api.SyncApi
+import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.transferschedule.model.MoveTransfersRequest
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.transferschedule.model.ReferenceId
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.transferschedule.model.ResyncResponse
 import uk.gov.justice.digital.hmpps.prisonerfromnomismigration.transferschedule.model.ResyncTransfersRequest
@@ -34,4 +36,8 @@ class TransferScheduleDpsApiService(@Qualifier("transferScheduleDpsApiWebClient"
   suspend fun resyncPrisoner(personIdentifier: String, request: ResyncTransfersRequest) = syncApi.prepare(syncApi.resyncRequestConfig(personIdentifier, request))
     .retrieve()
     .awaitBodyOrNullWhenNotFound<ResyncResponse>()
+
+  suspend fun moveBooking(request: MoveTransfersRequest) = syncApi.prepare(syncApi.moveTransfersRequestConfig(request))
+    .retrieve()
+    .awaitBodilessEntityOrLogAndRethrowBadRequest()
 }
